17cc2843f6c84de72f8dc9c0a7cdbfe92e2db0e1
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-05-22 10:54:31 +0200

    _

--- src/classes/DataContainer/__tests__/constructor.test.js
+++ src/classes/DataContainer/__tests__/constructor.test.js
@@ -1,25 +1,25 @@
-// import DataContainer from '../index.js'
+import DataContainer from '../index.js'
 
 // Column-oriented data
-test('valid column-oriented data throws no error', () => {
+// test('valid column-oriented data throws no error', () => {
 
-})
+// })
 
-test('column-oriented data with unequal column lengths throws an error', () => {
+// test('column-oriented data with unequal column lengths throws an error', () => {
   
-})
+// })
 
 test('valid column-oriented data loads unchanged', () => {
-  // const columnOrientedData = {
-  //   fruit: ['apple', 'banana', 'coconut'],
-  //   quantity: [1, 2, 3]
-  // }
+  const columnOrientedData = {
+    fruit: ['apple', 'banana', 'coconut'],
+    quantity: [1, 2, 3]
+  }
 
-  // let dataContainer = new DataContainer(columnOrientedData)
+  let dataContainer = new DataContainer(columnOrientedData)
 
-  // expect(dataContainer.data()).toEqual(columnOrientedData)
+  expect(dataContainer.data()).toEqual(columnOrientedData)
 })
 
-test('loaded column-oriented data not a reference to original data', () => {
+// test('loaded column-oriented data not a reference to original data', () => {
 
-})
+// })
