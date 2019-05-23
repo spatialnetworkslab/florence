17aa8cb79e913839e51c1ab74ad92109318b0dce
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-05-23 17:42:36 +0200

    test: added quantitative domain tests

--- src/classes/DataContainer/__tests__/domainsAndTypes/domains.test.js
+++ src/classes/DataContainer/__tests__/domainsAndTypes/domains.test.js
@@ -3,7 +3,21 @@ import DataContainer from '../../index.js'
 describe('calculating domains', () => {
   // quantitative
   test('the lowest and highest Numbers in a column are the domain', () => {
-    
+    const data = { quantity: [20, 1, -200, 50, 12] }
+    const dataContainer = new DataContainer(data)
+    const domain = dataContainer.domain('quantity')
+
+    expect(domain[0]).toBe(-200)
+    expect(domain[1]).toBe(50)
+  })
+
+  test('quantitative domain ignores Infinity', () => {
+    const data = { quantity: [-20, Infinity, 199, 31, -1, -Infinity] }
+    const dataContainer = new DataContainer(data)
+    const domain = dataContainer.domain('quantity')
+
+    expect(domain[0]).toBe(-20)
+    expect(domain[1]).toBe(199)
   })
 
   // categorical
