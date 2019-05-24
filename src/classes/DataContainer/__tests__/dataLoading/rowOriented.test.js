340aabecb27fe0e522df3c51f1b3156a8358a10f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-05-24 15:15:39 +0200

    test: trying to mock method calls, not working (WIP)

--- src/classes/DataContainer/__tests__/dataLoading/rowOriented.test.js
+++ src/classes/DataContainer/__tests__/dataLoading/rowOriented.test.js
@@ -1,42 +1,31 @@
 import DataContainer from '../../index.js'
 
-describe('loading row-oriented data', () => {
-  test('valid row-oriented data throws no error', () => {
+const mockSetRowDataframe = jest.fn()
+const mockSetColumnDataframe = jest.fn()
+jest.mock('../../index.js', () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      _setRowDataframe: mockSetRowDataframe,
+      _setColumnDataFrame: mockSetColumnDataframe
+    }
+  })
+})
+
+describe.skip('row-oriented data is converted to column-oriented data', () => {
+  beforeEach(() => {
+    DataContainer.mockClear()
+  })
+
+  test('valid row-oriented data calls right methods', () => {
     const validData = [
       { food: 'fries', rating: 7 },
       { food: 'kebab', rating: 8 },
       { food: 'nutella pancake', rating: 6 }
     ]
 
-    expect(() => new DataContainer(validData)).not.toThrow()
-  })
+    const dataContainer = new DataContainer(validData)
 
-  test('row-oriented data with unequal column lengths throws an error', () => {
-    const invalidData = [
-      { name: 'Tim', born: new Date(1994, 8, 7) },
-      { name: 'Tammy', born: new Date(1991, 3, 3) },
-      { born: new Date(2000, 2, 26) }
-    ]
-
-    expect(() => new DataContainer(invalidData)).toThrow()
-  })
-
-  test('columns are loaded correctly', () => {
-    const data = [
-      { food: 'fries', rating: 7 },
-      { food: 'kebab', rating: 8 },
-      { food: 'nutella pancake', rating: 6 }
-    ]
-
-    let dataContainer = new DataContainer(data)
-
-    expect(dataContainer.column('food')).toEqual(['fries', 'kebab', 'nutella pancake'])
-    expect(dataContainer.column('rating')).toEqual([7, 8, 6])
-  })
-
-  test('row-oriented data cannot be empty', () => {
-    const emptyData = []
-
-    expect(() => new DataContainer(emptyData)).toThrow()
+    expect(mockSetRowDataframe).toHaveBeenCalledTimes(1)
+    expect(mockSetColumnDataframe).toHaveBeenCalledTimes(1)
   })
 })
