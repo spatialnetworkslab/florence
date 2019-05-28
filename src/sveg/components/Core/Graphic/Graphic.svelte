11ef8bbdacb39ea6aa3a7390a205f276ebc67488
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-05-28 16:36:53 +0200

    refactor: Graphic component

--- src/sveg/components/Core/Graphic/Graphic.svelte
+++ src/sveg/components/Core/Graphic/Graphic.svelte
@@ -1,12 +1,15 @@
 <script>
     import  { setContext } from 'svelte'
-    import { coordinateContextKey } from '../contextKey.js'
-    import CoordinateContext from '../classes/CoordinateContext'
+    import { coordinateContextKey } from '../../contextKeys.js'
+    import CoordinateContext from '../../../classes/CoordinateContext'
 
     export let width
     export let height
 
-    const coordinateContext = new CoordinateContext()
+    const coordinateContext = new CoordinateContext({
+      rangeX: [0, width],
+      rangeY: [0, height]
+    })
 
     setContext(coordinateContextKey, coordinateContext)
 </script>
