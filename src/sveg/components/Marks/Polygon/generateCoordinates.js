ff6f824fe8a53cf2a05b7a77cf4261836ab53f28
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-06-17 17:43:59 +0200

    feat: generateCoordinates of Polygon (WIP)

--- src/sveg/components/Marks/Polygon/generateCoordinates.js
+++ src/sveg/components/Marks/Polygon/generateCoordinates.js
@@ -16,12 +16,19 @@ export function generateCoordinates (
   return transformedGeometry
 }
 
-function checkValidCombination (coordinateProps) {
+const invalidCombinationError = new Error(`Polygon: Invalid combination of 'x', 'y', and 'geometry' props`)
 
+function checkValidCombination (coordinateProps) {
+  if (coordinateProps.geometry) {
+    if (coordinateProps.x || coordinateProps.y) throw invalidCombinationError
+  } else {
+    if (!(coordinateProps.x && coordinateProps.y)) throw invalidCombinationError
+  }
 }
 
 function makeGeometry (coordinateProps) {
-
+  if (coordinateProps.geometry) {}
+  if (coordinateProps.x) {}
 }
 
 function scaleGeometry (geometry, sectionContext) {
