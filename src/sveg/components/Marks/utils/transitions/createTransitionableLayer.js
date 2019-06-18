44715651e205cd4e11de74c02524ebad5729e936
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-06-18 11:45:27 +0200

    chore: added some explanation to createTransitionable

--- src/sveg/components/Marks/utils/transitions/createTransitionableLayer.js
+++ src/sveg/components/Marks/utils/transitions/createTransitionableLayer.js
@@ -4,6 +4,18 @@ import { cubicOut } from 'svelte/easing'
 import { interpolateLayer } from './interpolateLayer.js'
 import { transitionGeometryLayer } from './geometry'
 
+/**
+ * Like createTransitionable, returns either a Svelte store, or a Svelte 'tweened' store,
+ * depending on whether the user specified transition options.
+ * But instead of for a single Mark, the store is created for an entire layer.
+ *
+ * @param {String} aestheticName The name of the aesthetic a store is created for.
+ * @param {*} aestheticValue The initial value of the store.
+ * @param {Number|Object} transitionOptions A number indicating the transtion duration, or an Object
+ * with aesthetic names as keys, and Numbers OR Objects as values.
+ * @param {String} [geometryType] For geometry aesthetics: the type of geometry.
+ * @returns {writable|tweened}
+ */
 export function createTransitionableLayer (aestheticName, aestheticValue, transitionOptions, geometryType) {
   if (transitionOptions === undefined) {
     return writable(aestheticValue)
