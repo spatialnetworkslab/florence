9995f7f83a53b61d4978448dc566e823b85a894e
Ate Poorthuis <atepoorthuis@gmail.com>
2019-07-15 00:27:44 +0800

    site: scaffolding for examples

--- site/src/components/Nav.svelte
+++ site/src/components/Nav.svelte
@@ -52,6 +52,7 @@
 	<ul>
 		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>home</a></li>
 		<li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a></li>
+		<li><a class='{segment === "examples" ? "selected" : ""}' href='examples'>examples</a></li>
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
