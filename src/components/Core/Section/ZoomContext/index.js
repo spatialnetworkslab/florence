ac5bc4a00a3bd4400ad742872c923c0102d02c19
Ate Poorthuis <atepoorthuis@gmail.com>
2019-07-14 13:38:09 +0800

    fix: remove stray d3-zoom import

--- src/components/Core/Section/ZoomContext/index.js
+++ src/components/Core/Section/ZoomContext/index.js
@@ -1,8 +1,6 @@
 import { getContext, setContext } from 'svelte'
 import { writable } from 'svelte/store'
 
-import { zoomIdentity } from 'd3-zoom'
-
 const key = {}
 
 export function subscribe () {
