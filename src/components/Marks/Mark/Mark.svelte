4bad1ebecb999a000e71e46a5785dcf594c1d45a
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-07-18 14:17:55 +0200

    feat: started on Mark component

---
+++ src/components/Marks/Mark/Mark.svelte
@@ -0,0 +1,169 @@
+<script context="module">
+  let idCounter = 0
+  function getId () {
+    return 'mark' + idCounter++
+  }
+</script>
+
+<script>
+  import { beforeUpdate, afterUpdate, onMount, onDestroy } from 'svelte'
+
+  import * as GraphicContext from '../../Core/Graphic/GraphicContext'
+  import * as SectionContext from '../../Core/Section/SectionContext'
+  import * as CoordinateTransformationContext from '../../Core/CoordinateTransformation/CoordinateTransformationContext'
+  import * as InteractionManagerContext from '../../Core/Section/InteractionManagerContext'
+  import * as ZoomContext from '../../Core/Section/ZoomContext'
+  
+  import { transformGeometry } from 'geometryUtils'
+  import { generateScreenGeometry, ensureValidPropCombination } from './generateScreenGeometry.js'
+  // import { createTransitionable, transitionsEqual } from '../utils/transitions'
+
+  let markId = getId()
+
+  let initPhase = true
+  const initDone = () => !initPhase
+
+  // Props
+  export let type
+
+  // Positioning props
+  export let x = undefined
+  export let y = undefined
+  export let x1 = undefined
+  export let x2 = undefined
+  export let y1 = undefined
+  export let y2 = undefined
+  export let geometry = undefined
+
+  // Other aesthetics
+  export let radius = 3
+  export let fill = 'black'
+
+  // Transitions/interactions
+  export let transition = undefined
+  export let onClick = undefined
+  export let onMouseover = undefined
+  export let onMouseout = undefined
+
+  // Contexts
+  const graphicContext = GraphicContext.subscribe()
+  const sectionContext = SectionContext.subscribe()
+  const coordinateTransformationContext = CoordinateTransformationContext.subscribe()
+  const interactionManagerContext = InteractionManagerContext.subscribe()
+  const zoomContext = ZoomContext.subscribe()
+
+  // Create screenGeometry
+  $: positioningProps = { x, y, x1, x2, y1, y2, geometry }
+  ensureValidPropCombination(type, positioningProps)
+
+  let unzoomedSceenGeometry = generateScreenGeometry(type)(
+    positioningProps, $sectionContext, $coordinateTransformationContext
+  )
+  let screenGeometry
+
+  // Initiate transitionables
+  let tr_screenGeometry = createTransitionable('geometry', getZoomedScreenGeometry(), transition)
+  let tr_radius = createTransitionable('radius', radius, transition)
+  let tr_fill = createTransitionable('fill', fill, transition)
+
+  // Handle zooming
+  $: {
+    if ($zoomContext) {
+      tr_screenGeometry.set(getZoomedScreenGeometry())
+    }
+  }
+
+  // Handle screenGeometry transitions
+  $: {
+    if (initDone()) {
+      unzoomedSceenGeometry = generateScreenGeometry(
+        positioningProps, $sectionContext, $coordinateTransformationContext
+      )
+
+      tr_screenGeometry.set(getZoomedScreenGeometry())
+      tr_radius.set(radius)
+
+      updateInteractionManagerIfNecessary()
+    }
+  }
+
+  // Handle other transitions
+  $: { if (initDone()) tr_fill.set(fill) }
+
+  let previousTransition
+
+  // Update transitionables
+  beforeUpdate(() => {
+    if (!transitionsEqual(previousTransition, transition)) {
+      previousTransition = transition
+
+      tr_screenGeometry = createTransitionable('geometry', $tr_screenGeometry, transition)
+      tr_radius = createTransitionable('radius', $tr_radius, transition)
+      tr_fill = createTransitionable('fill', $tr_fill, transition)
+    }
+  })
+
+  afterUpdate(() => {
+    initPhase = false
+  })
+
+  // Interactivity
+  $: isInteractive = onClick !== undefined || onMouseover !== undefined || onMouseout !== undefined
+
+  onMount(() => {
+    updateInteractionManagerIfNecessary()
+  })
+
+  onDestroy(() => {
+    removeMarkFromSpatialIndexIfNecessary()
+  })
+
+  // Helpers
+  function getZoomedScreenGeometry () {
+    if ($zoomContext) {
+      screenGeometry = transformGeometry(unzoomedSceenGeometry, $zoomContext)
+    } else {
+      screenGeometry = unzoomedSceenGeometry
+    }
+
+    return screenGeometry
+  }
+
+  function updateInteractionManagerIfNecessary () {
+    removeMarkFromSpatialIndexIfNecessary()
+
+    if (isInteractive) {
+      $interactionManagerContext.loadMark('Point', createMarkData())
+
+      if (onClick) $interactionManagerContext.addMarkInteraction('click', markId, onClick)
+      if (onMouseover) $interactionManagerContext.addMarkInteraction('mouseover', markId, onMouseover)
+      if (onMouseout) $interactionManagerContext.addMarkInteraction('mouseout', markId, onMouseout)
+    }
+  }
+
+  function removeMarkFromSpatialIndexIfNecessary () {
+    if ($interactionManagerContext.markIsLoaded(markId)) {
+      $interactionManagerContext.removeAllMarkInteractions(markId)
+      $interactionManagerContext.removeMark(markId)
+    }
+  }
+
+  function createMarkData () {
+    return {
+      attributes: { screenGeometry, radius },
+      markId
+    }
+  }
+</script>
+
+{#if $graphicContext.output() === 'svg'}
+
+  <circle 
+    class="point"
+    cx={$tr_screenGeometry.coordinates[0]} 
+    cy={$tr_screenGeometry.coordinates[1]} 
+    r={$tr_radius} 
+    fill={$tr_fill} 
+  />
+
+{/if}
