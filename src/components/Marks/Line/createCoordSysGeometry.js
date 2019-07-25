5f0771e4d4573c8c077f0e180f0bebeee9f69b98
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-07-25 11:08:32 +0200

    feat: put shared logic of createCoordSysGeometry of Polygon and Line in own module

--- src/components/Marks/Line/createCoordSysGeometry.js
+++ src/components/Marks/Line/createCoordSysGeometry.js
@@ -1,6 +1,8 @@
 import { scaleGeometry } from 'geometryUtils'
 import { createCoordSysGeometry } from '../utils/createCoordSysGeometry.js'
 import { isDefined, isUndefined } from 'equals.js'
+import createScaledGeometryFromXYProps, { ensureValidCombination }
+  from '../utils/createScaledGeometryFromXYProps.js'
 
 export default function (geometryProps, sectionContext, coordinateTransformationContext) {
   const scaledGeometry = createScaledGeometry(geometryProps, sectionContext)
@@ -10,88 +12,14 @@ export default function (geometryProps, sectionContext, coordinateTransformation
 }
 
 function createScaledGeometry (geometryProps, sectionContext) {
-  ensureValidCombination(geometryProps)
+  ensureValidCombination(geometryProps, 'Line')
+  const scales = sectionContext.scales()
 
   if (isDefined(geometryProps.geometry)) {
-    return scaleGeometryProp(geometryProps.geometry, sectionContext)
+    return scaleGeometry(geometryProps.geometry, scales)
   }
 
   if (isUndefined(geometryProps.geometry)) {
-    return createScaledGeometryFromCoordinateProps(geometryProps.x, geometryProps.y, sectionContext)
+    return createScaledGeometryFromXYProps(geometryProps.x, geometryProps.y, scales, 'Line')
   }
 }
-
-export function ensureValidCombination (geometryProps) {
-  if (isDefined(geometryProps.geometry)) {
-    if (isDefined(geometryProps.x) || isDefined(geometryProps.y)) throw invalidCombinationError
-  } else {
-    if (!(isDefined(geometryProps.x) && isDefined(geometryProps.y))) throw invalidCombinationError
-  }
-}
-
-const invalidCombinationError = new Error(`Point: invalid combination of props 'x', 'y' and 'geometry'`)
-
-function scaleGeometryProp (geometry, sectionContext) {
-  return scaleGeometry(geometry, sectionContext.scales())
-}
-
-function createScaledGeometryFromCoordinateProps (x, y, sectionContext) {
-  const scales = sectionContext.scales()
-  const scaledX = getValueX(x, scales)
-  const scaledY = getValueY(y, scales)
-
-  return createGeometryFromScaledProps(scaledX, scaledY)
-}
-
-function makeValueGetter (scaleName) {
-  return function getValue (coordinateProp, scales, length) {
-    const scale = scales[scaleName]
-
-    if (coordinateProp.constructor === Function) {
-      return handleFunctionProp(coordinateProp, scales, length)
-    } else {
-      return handleOtherProp(coordinateProp, scale, length)
-    }
-  }
-}
-
-const getValueX = makeValueGetter('scaleX')
-const getValueY = makeValueGetter('scaleY')
-
-function handleFunctionProp (coordinateProp, scales) {
-  const value = coordinateProp(scales)
-  if (value.constructor === Array) return value
-
-  throw noArrayError
-}
-
-function handleOtherProp (coordinateProp, scale, length) {
-  if (coordinateProp.constructor === Array) return coordinateProp.map(scale)
-
-  throw noArrayError
-}
-
-const noArrayError = new Error(`Line: 'x' and 'y' must evaluate to an Array`)
-
-export function createGeometryFromScaledProps (x, y) {
-  ensureCorrectLength(x, y)
-
-  const lineStringCoordinates = []
-
-  for (let i = 0; i < x.length; i++) {
-    lineStringCoordinates.push([x[i], y[i]])
-  }
-
-  return {
-    type: 'LineString',
-    coordinates: lineStringCoordinates
-  }
-}
-
-function ensureCorrectLength (x, y) {
-  if (x.length !== y.length) throw notSameLengthError
-  if (x.length < 2) throw notEnoughPointsError
-}
-
-const notSameLengthError = new Error(`Line: 'x' and 'y' must have same length`)
-const notEnoughPointsError = new Error('Line: must consist of at least 2 points')
---
+++ src/components/Marks/utils/createScaledGeometryFromXYProps.js
@@ -0,0 +1,110 @@
+import { linearRingIsClockwise } from 'geometryUtils'
+import { isDefined } from 'equals.js'
+
+export default function createScaledGeometryFromXYProps (x, y, scales, markType) {
+  const scaledX = getValueX(x, scales, markType)
+  const scaledY = getValueY(y, scales, markType)
+
+  return createGeometryFromScaledProps(scaledX, scaledY, markType)
+}
+
+function makeValueGetter (scaleName) {
+  return function getValue (coordinateProp, scales) {
+    const scale = scales[scaleName]
+
+    if (coordinateProp.constructor === Function) {
+      return handleFunctionProp(coordinateProp, scales)
+    } else {
+      return handleOtherProp(coordinateProp, scale)
+    }
+  }
+}
+
+const getValueX = makeValueGetter('scaleX')
+const getValueY = makeValueGetter('scaleY')
+
+function handleFunctionProp (coordinateProp, scales, markType) {
+  const value = coordinateProp(scales)
+  if (value.constructor === Array) return value
+
+  throw noArrayError(markType)
+}
+
+function handleOtherProp (coordinateProp, scale, markType) {
+  if (coordinateProp.constructor === Array) return coordinateProp.map(scale)
+
+  throw noArrayError(markType)
+}
+
+const noArrayError = markType => new Error(`${markType}: 'x' and 'y' must evaluate to an Array`)
+
+export function createGeometryFromScaledProps (x, y, markType) {
+  ensureCorrectLength(x, y, markType)
+
+  const coordinates = []
+
+  for (let i = 0; i < x.length; i++) {
+    coordinates.push([x[i], y[i]])
+  }
+
+  if (markType === 'Polygon') {
+    return createPolygon(coordinates)
+  }
+
+  if (markType === 'Line') {
+    return createLineString(coordinates)
+  }
+}
+
+function ensureCorrectLength (x, y, markType) {
+  if (x.length !== y.length) throw notSameLengthError(markType)
+  if (markType === 'Polygon' && x.length < 3) throw notEnoughPointsError(markType, 3)
+  if (markType === 'Line' && x.length < 2) throw notEnoughPointsError(markType, 2)
+}
+
+const notSameLengthError = markType => new Error(`${markType}: 'x' and 'y' must have same length`)
+const notEnoughPointsError = (markType, numberOfPoints) => {
+  return new Error(`${markType}: must consist of at least ${numberOfPoints} points`)
+}
+
+function createPolygon (coordinates) {
+  // To adhere to the GeoJSON spec, outer rings must always be closed
+  if (isNotClosed(coordinates)) {
+    const firstPosition = coordinates[0]
+    coordinates.push([firstPosition[0], firstPosition[1]])
+  }
+
+  // To adhere to the GeoJSON spec, outer rings must always be counter-clockwise
+  if (linearRingIsClockwise(coordinates)) {
+    coordinates.reverse()
+  }
+
+  return {
+    type: 'Polygon',
+    coordinates: [coordinates]
+  }
+}
+
+function createLineString (coordinates) {
+  return {
+    type: 'LineString',
+    coordinates
+  }
+}
+
+function isNotClosed (ring) {
+  const first = ring[0]
+  const last = ring[ring.length - 1]
+
+  return first[0] !== last[0] || first[1] !== last[1]
+}
+
+export function ensureValidCombination (geometryProps, markType) {
+  if (isDefined(geometryProps.geometry)) {
+    if (isDefined(geometryProps.x) || isDefined(geometryProps.y)) throw invalidCombinationError(markType)
+  } else {
+    if (!(isDefined(geometryProps.x) && isDefined(geometryProps.y))) throw invalidCombinationError(markType)
+  }
+}
+
+const invalidCombinationError = mark => new Error(`${mark}: Invalid combination of 'x', 'y', and 'geometry' props`)
