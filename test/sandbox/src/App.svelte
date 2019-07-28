9d9d937624fe6c9b67b5be7beab645e30f7343d3
Ate Poorthuis <atepoorthuis@gmail.com>
2019-07-28 17:19:56 +0800

    chore: initial geo polygon sandbox test

--- test/sandbox/src/App.svelte
+++ test/sandbox/src/App.svelte
@@ -9,6 +9,7 @@
   import Scatterplot from './examples/Scatterplot.svelte'
   import Zoom from './examples/Zoom.svelte'
   import BaseMark from './examples/BaseMark.svelte'
+  import GeoPolygons from './examples/GeoPolygons.svelte'
 
   // and add them to this component array
   let components = [
@@ -46,6 +47,11 @@
       name: 'Base mark',
       url: '/basemark',
       component: BaseMark
+    },
+     {
+      name: 'Geo Polygons',
+      url: '/geopolygons',
+      component: GeoPolygons
     }
   ]
 </script>
---
+++ test/sandbox/src/examples/GeoPolygons.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { scaleLinear } from 'd3-scale'
+  import { Graphic, Section, CoordinateTransformation, Polygon, PolygonLayer, DataContainer } from '../../../../src/'
+
+  // import data
+  import hex from '../data/hexagons.json'
+  
+</script>
+
+<Graphic width={500} height={500}>
+
+  <Section
+    x1={50} x2={450}
+    y1={50} y2={450}
+    scaleX={scaleLinear().domain([100, 110])}
+    scaleY={scaleLinear().domain([-1, 1])}
+  >
+
+    <PolygonLayer 
+      geometry={hex.features}
+      opacity='0.5'
+    />
+  
+  </Section>
+
+</Graphic>
