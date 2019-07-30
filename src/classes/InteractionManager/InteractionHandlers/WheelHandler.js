914afa85afd7647f45e41086098f5c25b8489095
Bianchi <bianchi_dy@sutd.edu.sg>
2019-07-31 00:35:59 +0800

    feat: implement sidescroll and cross-browser deltaMode

--- src/classes/InteractionManager/InteractionHandlers/WheelHandler.js
+++ src/classes/InteractionManager/InteractionHandlers/WheelHandler.js
@@ -20,9 +20,33 @@ export default class WheelHandler extends SectionInteractionHandler {
     }
   }
 
+  // normalised for all browsers, trackpads and mouses
+  // based on openstreemtmaps: https://github.com/openstreetmap/iD/blob/f61c482188b1b747fdf528ac2992f6ed9e8a2b6a/modules/renderer/map.js#L376-L396
+  // and normalize-wheel: https://github.com/basilfx/normalize-wheel/blob/master/src/normalizeWheel.js
+  // Key data: This code tries to resolve a single slow step on a wheel to 1.
+  // Why 10 and 40? Because they are considered reasonable line height defaults
   _defaultWheelDelta (event) {
-    // normalised for all browsers
-    return -event.deltaY * (event.deltaMode ? scrollLineHeight : 1) / 500
+    let k
+    let delta
+
+    // Legacy
+    if ('detail' in event) { delta = event.detail }
+
+    if ('deltaY' in event && event.deltaY !== 0) {
+      delta = -event.deltaY
+    } else if ('deltaX' in event && event.deltaX !== 0) {
+      delta = -event.deltaX
+    }
+
+    if (event.deltaMode === 1 /* LINE */) {
+      // Pick sensible scroll amount based on if user scrolling fast or slow.
+      var lines = Math.abs(event.deltaY)
+      var scroll = lines > 2 ? 40 : lines * 10
+      k = Math.pow(2, delta * scroll / 500)
+    } else {
+      k = delta * (event.deltaMode ? scrollLineHeight : 1) / 500
+    }
+    return k
   }
 
   _nopropagation (event) {
