6e6fa1521a4e9607cc39814cdc68484f8cd7e2a4
Bianchi <bianchi_dy@sutd.edu.sg>
2019-07-30 17:59:16 +0800

    feat: apply offset when zoom is within range to prevent jitter

--- src/components/Interactions/createZoomHandler.js
+++ src/components/Interactions/createZoomHandler.js
@@ -7,23 +7,26 @@ export default function createZoomHandler (zoomId, minZoom, maxZoom, extents, st
     const delta = event.wheelDelta * step
     const tempK = zoomId.k - delta
 
+    // Offsetting only takes effect when k is within range to prevent jitter
     if (tempK >= minZoom && tempK <= maxZoom) {
       zoomId.k -= delta
-    }
 
-    // stops zooming if past extents X and Y
-    const offsetX = -(event.coordinates.x * delta)
-    const offsetY = -(event.coordinates.y * delta)
+      // stops zooming if past extents X and Y
+      const offsetX = -(event.coordinates.x * delta)
+      const offsetY = -(event.coordinates.y * delta)
 
-    const tempX = zoomId.x - offsetX
-    const tempY = zoomId.y - offsetY
+      const tempX = zoomId.x - offsetX
+      const tempY = zoomId.y - offsetY
 
-    if ((tempX <= extentX[1] && tempX >= extentX[0]) && (tempY <= extentY[1] && tempY >= extentY[0])) {
-      zoomId.x -= offsetX
-      zoomId.y -= offsetY
-    } else {
-      zoomId.x += offsetX
-      zoomId.y += offsetY
+      // Make sure the viewport stays on the cursor area when zooming in/out
+      if ((tempX <= extentX[1] && tempX >= extentX[0]) && (tempY <= extentY[1] && tempY >= extentY[0])) {
+        zoomId.x -= offsetX
+        zoomId.y -= offsetY
+      } else {
+        zoomId.x += offsetX
+        zoomId.y += offsetY
+      }
+
     }
 
     return zoomId
@@ -36,7 +39,7 @@ export default function createZoomHandler (zoomId, minZoom, maxZoom, extents, st
     zoomId.k = 1
     return zoomId
   }
-  
+
   // Brings viewport back to specified center point
   const center = function () {
     zoomId.x = centerPt.x
@@ -49,5 +52,3 @@ export default function createZoomHandler (zoomId, minZoom, maxZoom, extents, st
 
   return handler
 }
-
-
