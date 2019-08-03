49565a159086c12cbf03de2b2880ed0474ee7f26
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-08-03 20:20:36 +0200

    feat: set up example to test line transtions

--- test/sandbox/src/examples/Line.svelte
+++ test/sandbox/src/examples/Line.svelte
@@ -1,39 +1,51 @@
 <script>
-  import { scaleLinear } from 'd3-scale'
-  import { Graphic, Section, Line, LineLayer } from '../../../../src/'
-  import DataContainer from '@snlab/florence-datacontainer'
+  import { scaleLinear } from "d3-scale";
+  import { Graphic, Section, Line, LineLayer } from "../../../../src/";
+  import DataContainer from "@snlab/florence-datacontainer";
 
   const data = new DataContainer({
     x: [1, 2, 4, 6, 9, 10, 12, 13],
     y: new Array(8).fill(0).map((_, i) => Math.random() * i)
-  })
+  });
 
   const multiLineString = {
-    type: 'MultiLineString',
+    type: "MultiLineString",
     coordinates: [
-      [[0, 0], [2, 8], [9, 9]],
+      [[0, 0], [2, 8], [9, 9]], 
       [[1, 1], [4, 4], [1, 4], [4, 1]]
     ]
-  }
+  };
+
+  const multiLineString2 = {
+    type: "MultiLineString",
+    coordinates: [
+      [[0, 0], [4, 4], [8, 8]],
+      [[2, 0], [6, 1], [8, 2]]
+    ]
+  };
 
   let clicked = false
+  let geometry = clicked ?
+    multiLineString2 :
+    multiLineString
 </script>
 
 <Graphic width={500} height={500}>
 
   <Section
-    x1={50} x2={450}
-    y1={50} y2={450}
+    x1={50}
+    x2={450}
+    y1={50}
+    y2={450}
     scaleX={scaleLinear().domain(data.domain('x'))}
     scaleY={scaleLinear().domain(data.domain('y'))}
-    padding={10}
-  >
+    padding={10}>
 
     <Line
       geometry={multiLineString}
       strokeWidth={2}
-      stroke={clicked ? 'green': 'red'}
-      onClick={clicked = true}
+      stroke={clicked ? 'green' : 'red'}
+      onClick={() => { clicked = !clicked }} 
     />
 
     <LineLayer
@@ -41,9 +53,9 @@
       y={[data.column('y')]}
       strokeWidth={10}
       stroke={clicked ? 'green' : 'red'}
-      onClick={clicked = true}
+      onClick={() => { clicked = !clicked }} 
     />
-  
+
   </Section>
 
 </Graphic>
