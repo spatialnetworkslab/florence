2de6239311d77d2b947fc1600808440a1c4fb465
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-08-06 11:35:17 +0200

    _

--- src/components/Marks/FuncLine/createScreenGeometry.js
+++ src/components/Marks/FuncLine/createScreenGeometry.js
@@ -1,15 +1,42 @@
 import { interpolate } from 'd3-interpolate'
 
 export default function createScreenGeometry (func, sectionContext, coordinateTransformationContext, zoomContext) {
-  
+  const domains = findDomains(sectionContext, coordinateTransformationContext, zoomContext)
+  const dataPoints = interpolatePointsFromFunc(func, domains)
+
+  return {
+    type: 'LineString',
+    coordinates: dataPoints
+  }
 }
 
-function findDomains () {
-  const domains = {}
-
-  // TODO
+function findDomains (sectionContext, coordinateTransformationContext, zoomContext) {
+  const screenRanges = getScreenRanges(sectionContext)
+  const rangeExtentsBeforeZoom = invertZoomRanges(screenRanges, zoomContext)
+  const domains = getDomains(rangeExtentsBeforeZoom, sectionContext, coordinateTransformationContext)
 
   return domains
+}
+
+function getScreenRanges (sectionContext) {
+  return {
+    x: [sectionContext.x1(), sectionContext.x2()],
+    y: [sectionContext.y1(), sectionContext.y2()]
+  }
+}
+
+function invertZoomRanges (screenRanges, zoomContext) {
+  const p0 = zoomContext.invert([screenRanges.x[0], screenRanges.y[0]])
+  const p1 = zoomContext.invert([screenRanges.x[1], screenRanges.y[1]])
+
+  return {
+    x: [p0[0], p1[0]],
+    y: [p0[1], p1[1]]
+  }
+}
+
+function getDomains (preZoomRanges, sectionContext, coordinateTransformationContext) {
+  // TODO
 }
 
 function interpolatePointsFromFunc (func, domains, resolution = 100) {
