f066bfa4e62a6d0f0b9de0b6612484808d7c46b3
Ate Poorthuis <atepoorthuis@gmail.com>
2019-08-10 23:14:59 +0800

    fix: axes integration test

--- test/integration/tests/components/axes.spec.js
+++ test/integration/tests/components/axes.spec.js
@@ -16,11 +16,11 @@ context('Assertions', () => {
     })
 
     it('[render] axis has tick marks', () => {
-      cy.get('g.x-axis > .line-layer > .line').should('have.length.greaterThan', 8)
+      cy.get('g.x-axis > .line-layer > .line').should('have.length.greaterThan', 6)
     })
 
     it('[render] axis has tick labels', () => {
-      cy.get('g.x-axis > .label-layer > .label').should('have.length.greaterThan', 8)
+      cy.get('g.x-axis > .label-layer > .label').should('have.length.greaterThan', 6)
     })
   })
 })
