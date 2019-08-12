33ae76ff5f180a86613bb1b550757db6b0068a6e
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-08-12 17:08:59 +0200

    feat: supporting .invert() for categorical scales (band, point)

--- src/components/Core/Section/SectionContext/index.js
+++ src/components/Core/Section/SectionContext/index.js
@@ -13,10 +13,27 @@ class SectionContext {
     this.y1 = this.rangeY[0]
     this.y2 = this.rangeY[1]
 
-    this._scaleX = scaleX ? scaleX.copy().range(rangeX) : x => x
-    this._scaleY = scaleY ? scaleY.copy().range(rangeY) : y => y
+    this._handleScales(scaleX, scaleY, rangeX, rangeY)
 
     this._padding = padding
+  }
+
+  _handleScales (scaleX, scaleY, rangeX, rangeY) {
+    if (scaleX) {
+      this._scaleX = scaleX.copy().range(rangeX)
+      this._scaleX.invert = createInvertMethod(this._scaleX)
+    } else {
+      this._scaleX = x => x
+      this._scaleX.invert = x => x
+    }
+
+    if (scaleY) {
+      this._scaleY = scaleY.copy().range(rangeY)
+      this._scaleY.invert = createInvertMethod(this._scaleY)
+    } else {
+      this._scaleY = y => y
+      this._scaleY.invert = y => y
+    }
   }
 
   scales () {
@@ -45,4 +62,31 @@ export function init () {
 
 export function update (sectionContext, options) {
   sectionContext.set(new SectionContext(options))
+}
+
+/**
+ * Taken from react-vis:
+ * https://github.com/uber/react-vis/blob/master/src/utils/scales-utils.js#L161
+ */
+function createInvertMethod (scale) {
+  if (scale.invert) {
+    return scale.invert
+  }
+
+  return function invert (value) {
+    const [lower, upper] = scale.range()
+    const start = Math.min(lower, upper)
+    const stop = Math.max(lower, upper)
+
+    if (value < start + scale.padding() * scale.step()) {
+      return scale.domain()[0]
+    }
+
+    if (value > stop - scale.padding() * scale.step()) {
+      return scale.domain()[scale.domain().length - 1]
+    }
+
+    const index = Math.floor((value - start - scale.padding() * scale.step()) / scale.step())
+    return scale.domain()[index]
+  }
 }
