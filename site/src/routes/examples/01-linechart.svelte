7e507c6d581c1cbd8b15879f9712c149647a55da
johsi-k <johsi.k@gmail.com>
2019-08-23 16:35:36 +0800

    refactor: remove unnecessary axes props

--- site/src/routes/examples/01-linechart.svelte
+++ site/src/routes/examples/01-linechart.svelte
@@ -35,46 +35,6 @@
   // group data by symbol so we can plot one line per group
   const groupedData = loadedData.groupBy('symbol').done()
 
-  let options = {
-    flip: 'false',
-    vjust: 'bottom',
-    y: undefined,
-    yOffset: 0,
-    ticks: 'true',
-    tickCount: '8',
-    tickValues: '',
-    tickSize: 5,
-    tickWidth: 0.5,
-    tickColor: 'black',
-    tickOpacity: 1,
-    tickExtra: 'false',
-    baseLine: 'true',
-    baseLineColor: 'black',
-    baseLineOpacity: 1,
-    baseLineWidth: 1,
-    labelFormat: undefined,
-    labelOffset: 2,
-    labelRotate: 0,
-    labelFont: "Helvetica",
-    labelFontSize: 10,
-    labelFontWeight: 'normal',
-    labelOpacity: 1,
-    labelColor: 'black',
-    titleHjust: 'center',
-    titleXOffset: 0,
-    titleYOffset: 'axis',
-    titleVjust: 'axis',
-    title: 'date',
-    titleColor: 'black',
-    titleFont: 'Helvetica',
-    titleFontSize: '12',
-    titleFontWeight: 'bold',
-    titleOpacity: 1,
-    titleRotation: 0,
-    titleAnchorPoint: 't',
-    transition: 2000
-  }
-
 </script>
 
 <Graphic 
@@ -83,7 +43,7 @@
 >
   <Label
     x={250}
-    y={25}
+    y={15}
     text={'Stock prices of 5 tech companies over time'}
   />
 
@@ -99,52 +59,15 @@
       y={groupedData.map('$grouped', group => group.column('price'))}
       stroke={groupedData.map('$grouped', group => scaleColour(group.column('symbol')))}
     />
-
+    
     <XAxis
-      yOffset={Number(options.yOffset)}
-      flip={options.flip === 'true'}
-      vjust={isNaN(options.vjust) ? options.vjust : Number(options.vjust)}
-      y={Number(options.y)}
-      baseLine={options.baseLine === 'true'}
-      ticks={options.ticks === 'true'}
-      tickCount={Number(options.tickCount)}
-      tickValues={(options.tickValues.length > 0) ? options.tickValues.split(',') : []}
-      tickSize={Number(options.tickSize)}
-      tickWidth={Number(options.tickWidth)}
-      tickColor={options.tickColor}
-      tickOpacity={Number(options.tickOpacity)}
-      tickExtra={options.tickExtra === 'true'}
-      baseLineOpacity={Number(options.baseLineOpacity)}
-      baseLineWidth={Number(options.baseLineWidth)}
-      baseLineColor={options.baseLineColor}
-      labelFormat = {options.labelFormat}
-      labelOffset = {Number(options.labelOffset)}
-      labelRotate = {Number(options.labelRotate)}
-      labelFont = {options.labelFont}
-      labelFontSize = {Number(options.labelFontSize)}
-      labelFontWeight = {options.labelFontWeight}
-      labelOpacity = {Number(options.labelOpacity)}
-      labelColor={options.labelColor}
-      titleHjust={options.titleHjust}
-      titleXOffset={Number(options.titleXOffset)}
-      titleYOffset={options.titleYOffset}
-      titleVjust={options.titleVjust}
-      title={options.title}
-      titleColor={options.titleColor}
-      titleFont={options.titleFont}
-      titleFontSize={Number(options.titleFontSize)}
-      titleFontWeight={options.titleFontWeight}
-      titleOpacity={Number(options.titleOpacity)}
-      titleRotation={Number(options.titleRotation)}
-      titleAnchorPoint={options.titleAnchorPoint}
-      transition={Number(options.transition)}
+      title={'date'}
+      titleFontWeight={'bold'}
     />
 
     <YAxis
-      hjust={'left'}
-      baseLineWidth={1}
       title='price'
-      titleFontWeight={options.titleFontWeight}
+      titleFontWeight={'bold'}
     />
 
   </Section>
--- site/src/routes/examples/03-scatterplot.svelte
+++ site/src/routes/examples/03-scatterplot.svelte
@@ -25,46 +25,6 @@
     .domain(dataContainer.domain('Origin'))
     .range(['#e45756', '#f58518', '#4c78a8'])
 
-  let options = {
-    flip: 'false',
-    vjust: 'bottom',
-    y: undefined,
-    yOffset: 0,
-    ticks: 'true',
-    tickCount: '8',
-    tickValues: '',
-    tickSize: 5,
-    tickWidth: 0.5,
-    tickColor: 'black',
-    tickOpacity: 1,
-    tickExtra: 'false',
-    baseLine: 'true',
-    baseLineColor: 'black',
-    baseLineOpacity: 1,
-    baseLineWidth: 1,
-    labelFormat: undefined,
-    labelOffset: 2,
-    labelRotate: 0,
-    labelFont: "Helvetica",
-    labelFontSize: 10,
-    labelFontWeight: 'normal',
-    labelOpacity: 1,
-    labelColor: 'black',
-    titleHjust: 'center',
-    titleXOffset: 0,
-    titleYOffset: 'axis',
-    titleVjust: 'axis',
-    title: 'Horsepower',
-    titleColor: 'black',
-    titleFont: 'Helvetica',
-    titleFontSize: '12',
-    titleFontWeight: 'bold',
-    titleOpacity: 1,
-    titleRotation: 0,
-    titleAnchorPoint: 't',
-    transition: 2000
-  }
-
 </script>
 
 <Graphic 
@@ -87,57 +47,20 @@
    <PointLayer
      x={dataContainer.column('Horsepower')}
      y={dataContainer.column('Miles_per_Gallon')}
-     stroke={dataContainer.column('Origin').map(origin => scaleColour(origin))}
+     stroke={dataContainer.map('Origin', scaleColour)}
      fillOpacity={0}
      strokeWidth={2}
      radius={4}
    />
 
     <XAxis
-      yOffset={Number(options.yOffset)}
-      flip={options.flip === 'true'}
-      vjust={isNaN(options.vjust) ? options.vjust : Number(options.vjust)}
-      y={Number(options.y)}
-      baseLine={options.baseLine === 'true'}
-      ticks={options.ticks === 'true'}
-      tickCount={Number(options.tickCount)}
-      tickValues={(options.tickValues.length > 0) ? options.tickValues.split(',') : []}
-      tickSize={Number(options.tickSize)}
-      tickWidth={Number(options.tickWidth)}
-      tickColor={options.tickColor}
-      tickOpacity={Number(options.tickOpacity)}
-      tickExtra={options.tickExtra === 'true'}
-      baseLineOpacity={Number(options.baseLineOpacity)}
-      baseLineWidth={Number(options.baseLineWidth)}
-      baseLineColor={options.baseLineColor}
-      labelFormat = {options.labelFormat}
-      labelOffset = {Number(options.labelOffset)}
-      labelRotate = {Number(options.labelRotate)}
-      labelFont = {options.labelFont}
-      labelFontSize = {Number(options.labelFontSize)}
-      labelFontWeight = {options.labelFontWeight}
-      labelOpacity = {Number(options.labelOpacity)}
-      labelColor={options.labelColor}
-      titleHjust={options.titleHjust}
-      titleXOffset={Number(options.titleXOffset)}
-      titleYOffset={options.titleYOffset}
-      titleVjust={options.titleVjust}
-      title={options.title}
-      titleColor={options.titleColor}
-      titleFont={options.titleFont}
-      titleFontSize={Number(options.titleFontSize)}
-      titleFontWeight={options.titleFontWeight}
-      titleOpacity={Number(options.titleOpacity)}
-      titleRotation={Number(options.titleRotation)}
-      titleAnchorPoint={options.titleAnchorPoint}
-      transition={Number(options.transition)}
+      title={'Horsepower'}
+      titleFontWeight={'bold'}
     />
 
     <YAxis
-      hjust={'left'}
-      baseLineWidth={1}
       title='Miles per gallon'
-      titleFontWeight={options.titleFontWeight}
+      titleFontWeight={'bold'}
     />
 
   </Section>
