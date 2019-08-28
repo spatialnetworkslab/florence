8064e17ece54b4aef36d7e7ed14213a70aa57ba3
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-08-28 15:06:42 +0200

    _

---
+++ src/components/Core/utils/padding.js
@@ -0,0 +1,27 @@
+export function parsePadding (padding) {
+  if (padding.constructor === Number) {
+    return { left: padding, right: padding, top: padding, bottom: padding }
+  }
+
+  if (padding.constructor === Object) {
+    if (Object.keys(padding).length !== 4) throw invalidPaddingError
+
+    for (const key of ['left', 'right', 'top', 'bottom']) {
+      if (!(key in padding)) throw invalidPaddingError
+    }
+
+    return padding
+  }
+
+  throw invalidPaddingError
+}
+
+const invalidPaddingError = new Error('Invalid padding specification')
+
+export function applyPadding (range, offsetMin, offsetMax) {
+  if (range[0] < range[1]) {
+    return [range[0] + offsetMin, range[1] - offsetMax]
+  } else {
+    return [range[0] - offsetMax, range[1] + offsetMin]
+  }
+}
