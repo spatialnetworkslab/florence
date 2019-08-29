42684587d6a78044cd675f5f75124605000ba534
johsi-k <johsi.k@gmail.com>
2019-08-30 00:48:22 +0800

    examples: candlestick chart

---
+++ site/src/routes/examples/07-candlestick.svelte
@@ -0,0 +1,109 @@
+<script context="module">
+  import { csvParse  } from 'd3-dsv'
+  import { timeParse } from 'd3-time-format'
+  const parseDate = timeParse("%Y-%m-%d")
+
+  export async function preload() {
+    const response = await this.fetch('https://gist.githubusercontent.com/mbostock/696604b8395aa68d0e7dcd74abd21dbb/raw/55c17dab8461cde25ca8c735543fba839b0c940b/AAPL.csv')
+    const text = await response.text()
+    const data = csvParse(text, (d) => {
+      const date = parseDate(d["Date"])
+      return {
+        date,
+        high: +d["High"],
+        low: +d["Low"],
+        open: +d["Open"],
+        close: +d["Close"],
+      }
+    }).slice(-120)
+
+    return { data }
+  }
+</script>
+
+<script>
+  import { scaleLinear, scaleLog, scaleBand } from 'd3-scale'
+  import { interpolateRound } from 'd3-interpolate'
+  import { timeDay, timeMonday } from 'd3-time'
+  import { timeFormat } from 'd3-time-format'
+  import { Graphic, Section, Label, PointLayer, LineLayer, XAxis, YAxis } from '../../../../src/'
+  import DataContainer from '@snlab/florence-datacontainer'
+
+  export let data
+  const padding = { top: 20, bottom: 30, left: 40, right: 30 }
+
+  // set up data container 
+  const dataContainer = new DataContainer(data)
+
+  const domainHigh = dataContainer.domain('high')
+  const domainLow = dataContainer.domain('low')
+  const domainDate = dataContainer.domain('date')
+
+  // set scales
+  const scaleX = scaleBand()
+    .domain(timeDay
+      .range(domainDate[0], domainDate[1])
+      .filter(d => d.getDay() !== 0 && d.getDay() !== 6))
+    .padding(0.2)
+  const scaleY = scaleLog()
+    .domain([domainLow[0], domainHigh[1]])
+    .interpolate(interpolateRound)
+  
+  // configure axes
+  const xTicks = timeMonday
+    .every(1)
+    .range(domainDate[0], domainDate[1])
+  const scaleYAxis = scaleLinear().domain([domainLow[0], domainHigh[1]]).range([600-padding.bottom, padding.top])
+
+  // group data by symbol so as to plot one line per group
+  const groupedData = dataContainer.groupBy('high')
+
+</script>
+
+<Graphic
+  width={800}
+  height={600}
+>
+
+  <Label
+    x={400}
+    y={10}
+    text={'Candlestick Chart'}
+  />
+
+  <Section
+    scaleX={scaleX} 
+		scaleY={scaleY}
+    padding={{left: 40, right: 30, top: 20, bottom: 30}}
+    flipY
+  >  
+
+    <LineLayer
+      x={groupedData.map('$grouped', group => [group.column('date')[0], group.column('date')[0]])}
+      y={groupedData.map('$grouped', group => [group.column('low')[0], group.column('high')[0]])}
+      strokeWidth={1}
+    />
+    
+    <LineLayer
+      x={groupedData.map('$grouped', group => [group.column('date'), group.column('date')])}
+      y={groupedData.map('$grouped', group => [group.column('open'), group.column('close')])}
+      strokeWidth={4}
+      stroke={groupedData.map('$grouped', group => group.column('open') > group.column('close') ? 'red'
+        : group.column('close') > group.column('open') ? 'green'
+        : 'grey')}
+    />
+
+    <XAxis
+      tickValues={xTicks}
+      labelFormat={timeFormat("%-m/%-d")}
+      baseLine={false}
+    /> 
+
+    <YAxis
+      scale={scaleYAxis}
+      baseLine={false}
+    />
+
+  </Section>
+
+</Graphic>
