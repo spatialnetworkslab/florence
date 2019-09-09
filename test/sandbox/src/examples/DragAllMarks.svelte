8ffb4c45b1b4a4154db705622dbecd14cc24f83d
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-09-09 17:06:27 +0100

    feat: drag example for Rectangle working

--- test/sandbox/src/examples/DragAllMarks.svelte
+++ test/sandbox/src/examples/DragAllMarks.svelte
@@ -1,29 +1,52 @@
 <script>
-  import { Graphic, Rectangle } from '../../../../src'
+  import { Graphic, Section, Rectangle } from '../../../../src'
   import { scaleLinear } from 'd3-scale'
 
   let rectanglePosition = { x: 1, y: 1 }
   let rectangleWH = { w: 2, h: 2 }
-  let dragOffset = { x: 0, y: 0 }
 
   $: rectangleCoords = {
-    x1: rectanglePosition.x + dragOffset.x, 
-    y1: rectanglePosition.y + dragOffset.y, 
-    x2: rectanglePosition.x + dragOffset.x + rectangleWH.w,
-    y2: rectanglePosition.y + dragOffset.y + rectangleWH.h
+    x1: rectanglePosition.x, 
+    y1: rectanglePosition.y, 
+    x2: rectanglePosition.x + rectangleWH.w,
+    y2: rectanglePosition.y + rectangleWH.h
+  }
+
+  let startDelta
+
+  function handleStart (event) {
+    // console.log(event)
+    let localCoordinates = event.localCoordinates
+    startDelta = { 
+      x: localCoordinates.x - rectanglePosition.x,
+      y: localCoordinates.y - rectanglePosition.y
+    }
+  }
+
+  function handleDrag (event) {
+    console.log(event)
+    let localCoordinates = event.localCoordinates
+    rectanglePosition = {
+      x: localCoordinates.x - startDelta.x,
+      y: localCoordinates.y - startDelta.y
+    }
   }
 </script>
 
-<Graphic 
-  width={500} height={500}
-  scaleX={scaleLinear().domain([0, 10])}
-  scaleY={scaleLinear().domain([0, 10])}
->
+<Graphic width={500} height={500}>
 
-  <Rectangle 
-    {...rectangleCoords} 
-    fill="green"
-    onDrag={event => { dragOffset = event.delta }}
-  />
+  <Section 
+    scaleX={scaleLinear().domain([0, 10])}
+    scaleY={scaleLinear().domain([0, 10])}
+  >
+  
+    <Rectangle 
+      {...rectangleCoords} 
+      fill="green"
+      onDragstart={handleStart}
+      onDrag={handleDrag}
+    />
+
+  </Section>
 
 </Graphic>
