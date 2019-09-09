bc60f049642496dfc5aaa743454098a2b95e5d2f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-09-09 16:55:03 +0100

    feat: added Drag event to Rectangle and made sandbox example

--- test/sandbox/src/App.svelte
+++ test/sandbox/src/App.svelte
@@ -16,6 +16,7 @@
   import FunctionAPI from './examples/FunctionAPI.svelte'
   import FlipTest from './examples/FlipTest.svelte'
   import NonNumericAxis from './examples/NonNumericAxis.svelte'
+  import DragAllMarks from './examples/DragAllMarks.svelte'
 
   // and add them to this component array
   let components = [
@@ -88,6 +89,11 @@
       name: 'Non numeric axis',
       url: 'nonnumericaxis',
       component: NonNumericAxis
+    },
+    {
+      name: 'Drag all marks',
+      url: 'dragallmarks',
+      component: DragAllMarks
     }
   ]
 </script>
---
+++ test/sandbox/src/examples/DragAllMarks.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Graphic, Rectangle } from '../../../../src'
+  import { scaleLinear } from 'd3-scale'
+
+  let rectanglePosition = { x: 1, y: 1 }
+  let rectangleWH = { w: 2, h: 2 }
+  let dragOffset = { x: 0, y: 0 }
+
+  $: rectangleCoords = {
+    x1: rectanglePosition.x + dragOffset.x, 
+    y1: rectanglePosition.y + dragOffset.y, 
+    x2: rectanglePosition.x + dragOffset.x + rectangleWH.w,
+    y2: rectanglePosition.y + dragOffset.y + rectangleWH.h
+  }
+</script>
+
+<Graphic 
+  width={500} height={500}
+  scaleX={scaleLinear().domain([0, 10])}
+  scaleY={scaleLinear().domain([0, 10])}
+>
+
+  <Rectangle 
+    {...rectangleCoords} 
+    fill="green"
+    onDrag={event => { dragOffset = event.delta }}
+  />
+
+</Graphic>
