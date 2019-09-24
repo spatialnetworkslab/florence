1478fe78d6e64a3760dbfaf0d3797411f0cbe59f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-09-24 16:54:26 +0100

    feat: indexRectangle for select

---
+++ src/interactivity/interactions/select/createIndexableData/indexRectangle.js
@@ -0,0 +1,45 @@
+import { calculateCentroid } from '../../../../utils/geometryUtils'
+import bboxPoint from './utils/bboxPoint.js'
+import createItemFromBBox from '../../../../utils/createItemFromBBox.js'
+
+export function indexRectangle (markData) {
+  const rectangleAttributes = markData.attributes
+
+  const centroid = calculateCentroid(rectangleAttributes.screenGeometry)
+  const bbox = bboxPoint(centroid)
+  const item = createItemFromBBox(bbox)
+
+  item.attributes = rectangleAttributes
+  item.markType = 'Rectangle'
+  item.markId = markData.markId
+
+  return item
+}
+
+export function indexRectangleLayer ({ layerAttributes, keyArray, layerId }) {
+  const items = []
+
+  for (let i = 0; i < keyArray.length; i++) {
+    const key = keyArray[i]
+
+    const rectangleAttributes = getRectangleAttributes(layerAttributes, key)
+
+    const centroid = calculateCentroid(rectangleAttributes.screenGeometry)
+    const bbox = bboxPoint(centroid)
+    const item = createItemFromBBox(bbox)
+
+    item.key = key
+    item.index = i
+    item.attributes = rectangleAttributes
+    item.markType = 'Rectangle'
+    item.layerId = layerId
+
+    items.push(item)
+  }
+
+  return items
+}
+
+function getRectangleAttributes (layerAttributes, key) {
+  return { screenGeometry: layerAttributes.screenGeometryObject[key] }
+}
