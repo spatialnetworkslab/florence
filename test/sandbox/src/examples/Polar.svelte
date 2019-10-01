8acf552cc6e21a92683a2fd235ce4504bf9d03ce
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-10-01 13:52:01 +0100

    feat: started on polar example

---
+++ test/sandbox/src/examples/Polar.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { 
+    Graphic, Section, 
+    RectangleLayer,
+    XAxis, YAxis
+  } from '../../../../src/'
+
+  import DataContainer from '@snlab/florence-datacontainer'
+  import { scaleLinear, scaleBand } from 'd3-scale'
+
+  const data = new DataContainer({
+    a: [5, 2, 7, 3, 6],
+    b: [6, 5, 2, 11, 3],
+    c: [2, 6, 1, 5, 3]
+  }).rowCumSum(['a', 'b', 'c'], { asInterval: true })
+
+  // TODO
+</script>
