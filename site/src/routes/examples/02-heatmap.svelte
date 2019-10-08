4109c9a463a210eb67354177fae31dfc0ee745fb
johsi-k <johsi.k@gmail.com>
2019-10-08 11:06:33 +0800

    examples: heatmap

--- site/src/routes/examples/02-heatmap.svelte
+++ site/src/routes/examples/02-heatmap.svelte
@@ -1,3 +1,5 @@
+<!-- adapted from https://vega.github.io/vega-lite/examples/rect_binned_heatmap.html -->
+
 <script context="module">
   export async function preload() {
     // instead of dealing with promises we await async functions here
@@ -10,46 +12,78 @@
 </script>
 
 <script>
-  import { scaleLinear, scaleTime, scaleOrdinal } from 'd3-scale'
-  import { Graphic, Section, Label, LineLayer, XAxis, YAxis } from '../../../../src/'
+  import { scaleLinear, scaleSequential } from 'd3-scale'
+  import { interpolateYlGnBu } from 'd3-scale-chromatic'
+  import { Graphic, Section, Label, Rectangle, RectangleLayer, XAxis, YAxis } from '../../../../src/'
   import DataContainer from '@snlab/florence-datacontainer'
   
   export let data
+  
   let reformattedData = data.map(d => {
     return {
       Title: String(d.Title),
       Rotten_Tomatoes_Rating: d.Rotten_Tomatoes_Rating,
-      IMDB_Rating: d.IMDB_Rating,
+      IMDB_Rating: d.IMDB_Rating
     }
   })
 
-  // set up data container 
-  // const dataContainer = new DataContainer(
-  // { a: [1, 2, 3, 4, 5, 6, 7], b: [8, 9, 10, 11, 12, 13, 14] }
-  // )
-  // const binned = dataContainer.bin({ groupBy: 'a', method: 'EqualInterval', numClasses: 3 }).done()
+  const dataContainer = new DataContainer(reformattedData)
 
-  // const dataContainer = new DataContainer(reformattedData)
-  // const binned = dataContainer
-  //   .dropNA()
-  //   .bin({ groupBy: 'IMDB_Rating', method: 'EqualInterval', numClasses: 3 })
-  //   .done()
+  const binned = dataContainer
+    .dropNA()
+    .bin([
+      { groupBy: 'IMDB_Rating', method: 'EqualInterval', numClasses: 38 },
+      { groupBy: 'Rotten_Tomatoes_Rating', method: 'EqualInterval', numClasses: 20 }
+    ])
+    .summarise({ count: { IMDB_Rating: 'count' } })
 
-  // set scales based on ungrouped data
+  const imdbDomain = binned.domain('bins_IMDB_Rating') // [1.6, 9.2]
+  const rtDomain = binned.domain('bins_Rotten_Tomatoes_Rating') // [1, 100]
+  const countDomain = binned.domain('count')
 
-  // group data by symbol so we can plot one line per group
+  const scaleX = scaleLinear().domain(imdbDomain)
+  const scaleY = scaleLinear().domain(rtDomain)
+  const scaleColor = scaleSequential(interpolateYlGnBu).domain(countDomain)
 
 </script>
 
 <Graphic 
-  width={500}
-  height={500}
+  width={600}
+  height={400}
 >
 
+  <Label
+    x={300}
+    y={10}
+    text={'Table Binned Heat Map'}
+  />
+
   <Section
+    {scaleX}
+    {scaleY}
     padding={{left: 40, right: 25, top: 25, bottom: 40}}
     flipY
-  >  
+  >
+
+  {#each binned.rows() as bin}
+    <Rectangle
+      x1={bin['bins_IMDB_Rating'][0]}
+      x2={bin['bins_IMDB_Rating'][1]}
+      y1={bin['bins_Rotten_Tomatoes_Rating'][0]}
+      y2={bin['bins_Rotten_Tomatoes_Rating'][1]}
+      fill={scaleColor(bin['count'])}
+    />
+  {/each}
+
+  <XAxis
+    title={"IMDB Rating"}
+    baseLine={false}
+  />
+
+  <YAxis
+    title={"Rotten Tomatoes Rating"}
+    baseLine={false}
+  />
 
   </Section>
 
