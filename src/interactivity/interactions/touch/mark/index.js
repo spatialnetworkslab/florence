54bf04ec14349c351bf185efe55d317a70a7eb80
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-10-21 12:56:16 +0100

    fix: bug (wrong touchstart handler, accidently imported drag handler)

--- src/interactivity/interactions/touch/mark/index.js
+++ src/interactivity/interactions/touch/mark/index.js
@@ -1,4 +1,4 @@
-export { default as TouchstartHandler } from './TouchdragHandler.js'
+export { default as TouchstartHandler } from './TouchstartHandler.js'
 export { default as TouchendHandler } from './TouchendHandler.js'
 
 export { default as TouchdragHandler } from './TouchdragHandler.js'
