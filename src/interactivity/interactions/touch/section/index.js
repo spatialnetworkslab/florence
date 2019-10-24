da86eb0ca988176fabbcb51b83c3ce5617ee8929
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-10-24 22:17:36 +0100

    feat: created TouchoverHandler and TouchoutHandler for Section

--- src/interactivity/interactions/touch/section/index.js
+++ src/interactivity/interactions/touch/section/index.js
@@ -1,3 +1,5 @@
 export { default as TouchdownHandler } from './TouchdownHandler.js'
 export { default as TouchmoveHandler } from './TouchmoveHandler.js'
 export { default as TouchupHandler } from './TouchupHandler.js'
+export { default as TouchoverHanlder } from './TouchoverHandler.js'
+export { default as TouchoutHandler } from './TouchoutHandler.js'
