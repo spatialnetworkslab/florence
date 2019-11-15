b8143954a63e2294d7db829ac8fd0f6bb064f250
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-11-15 16:50:56 +0000

    feat: PinchHandler should work now

--- src/interactivity/interactions/touch/section/PinchHandler.js
+++ src/interactivity/interactions/touch/section/PinchHandler.js
@@ -9,36 +9,99 @@ export default class PinchHandler extends SectionInteractionHandler {
   constructor (interactionManager) {
     super(interactionManager, {
       interactionName: 'pinch',
-      eventName: 'touchmove'
+      eventName: ['touchstart', 'touchmove', 'touchend']
     })
+
+    this._previousDelta = undefined
   }
 
-  _handleEvent (nativeEvent) {
-    if (numberOfTouches(nativeEvent) !== 2) {
+  _handleEvent (screenCoordinatesArray, nativeEvent) {
+    if (numberOfTouches(screenCoordinatesArray) !== 2) {
       return
     }
 
-    const interactionManager = this.interactionManager()
-    const section = this.section()
-
-    for (let i = 0; i < 2; i++) {
-      const touch = nativeEvent.touches[i]
+    if (nativeEvent.type === 'touchstart') {
+      this._handleTouchstart(screenCoordinatesArray, nativeEvent)
     }
 
-    // for ()
+    if (nativeEvent.type === 'touchmove') {
+      this._handleTouchmove(screenCoordinatesArray, nativeEvent)
+    }
 
-    // if (coordinatesAreInsideSection(screenCoordinates, section)) {
-    //   for (let i = 0; i < 2; i++) {
-    //     const touchPoint = nativeEvent.
-    //     const localCoordinates = getLocalCoordinates(screenCoordinates, interactionManager)
-
-    //     const pinchEvent = createSectionEvent('pinch', {
-    //       screenCoordinates,
-    //       localCoordinates
-    //     }, nativeEvent)
-
-    //     this._callback(pinchEvent)
-    //   }
-    // }
+    if (nativeEvent.type === 'touchend') {
+      this._handleTouchend(screenCoordinatesArray, nativeEvent)
+    }
   }
+
+  _handleTouchstart (screenCoordinatesArray, nativeEvent) {
+    // const interactionManager = this.interactionManager()
+    const section = this.section()
+
+    if (allCoordinatesAreInsideSection(screenCoordinatesArray, section)) {
+      const sectionHeight = section.maxY - section.minY
+      this._previousDelta = getDelta(screenCoordinatesArray, sectionHeight)
+    }
+  }
+
+  _handleTouchmove (screenCoordinatesArray, nativeEvent) {
+    if (!this._previousDelta) return
+
+    const section = this.section()
+
+    if (allCoordinatesAreInsideSection(screenCoordinatesArray, section)) {
+      const sectionHeight = section.maxY - section.minY
+
+      const center = getCenter(screenCoordinatesArray)
+
+      let delta = getDelta(screenCoordinatesArray, sectionHeight)
+      const absoluteDelta = Math.abs(delta)
+
+      if (this._previousDelta > absoluteDelta) {
+        delta = -delta
+      }
+
+      this._fireCallback(screenCoordinatesArray, nativeEvent, delta, center)
+
+      this._previousDelta = absoluteDelta
+    }
+  }
+
+  _handleTouchend (screenCoordinatesArray, nativeEvent) {
+    this._previousDelta = undefined
+  }
+
+  _fireCallback (screenCoordinatesArray, nativeEvent, delta, center) {
+    const screenCenter = center
+    const localCenter = getLocalCoordinates(screenCenter, this.interactionManager())
+    const screenCoordinates = screenCoordinatesArray
+    const localCoordinates = screenCoordinatesArray.map(screenCoordinates => {
+      return getLocalCoordinates(screenCoordinates, this.interactionManager())
+    })
+
+    const pinchEvent = createSectionEvent('pinch', {
+      screenCenter,
+      localCenter,
+      screenCoordinates,
+      localCoordinates,
+      delta
+    }, nativeEvent)
+
+    this._callback(pinchEvent)
+  }
+}
+
+function allCoordinatesAreInsideSection (screenCoordinatesArray, section) {
+  return screenCoordinatesArray.every(screenCoordinates => {
+    return coordinatesAreInsideSection(screenCoordinates, section)
+  })
+}
+
+function getDelta (screenCoordinatesArray, sectionHeight) {
+  const [coords1, coords2] = screenCoordinatesArray
+  return -Math.sqrt((coords2.x - coords1.x) ** 2 + (coords2.y - coords1.y) ** 2) / (sectionHeight * 50)
+}
+
+function getCenter (screenCoordinatesArray) {
+  const [coords1, coords2] = screenCoordinatesArray
+  return { x: (coords2.x + coords1.x) / 2, y: (coords2.y + coords1.y) / 2 }
 }
