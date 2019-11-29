1cb8f669e4f0dd4c186ff5f8c5304ee826d972fa
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-11-29 15:11:17 +0000

    rename: areaCollision -> symbolCollision

--- src/interactivity/interactions/SpatialIndex/collisionTests/symbolCollision.js
+++ src/interactivity/interactions/SpatialIndex/collisionTests/symbolCollision.js
@@ -1,6 +1,6 @@
 import { pointInPolygon } from '../../../../utils/geometryUtils'
 
-export default function areaCollision (coordinates, symbolAttributes) {
+export default function symbolCollision (coordinates, symbolAttributes) {
   const point = [coordinates.x, coordinates.y]
   return pointInPolygon(point, symbolAttributes.screenGeometry)
 }
