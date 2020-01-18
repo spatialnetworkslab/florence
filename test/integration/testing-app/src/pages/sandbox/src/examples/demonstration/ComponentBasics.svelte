8b300945f8d7ba43daab0069eea88b55d47de025
Ate Poorthuis <atepoorthuis@gmail.com>
2020-01-18 21:47:34 +0800

    fix: remove sandbox from integration test app

--- test/integration/testing-app/src/pages/sandbox/src/examples/demonstration/ComponentBasics.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import { Graphic, Section, Point, XAxis, YAxis } from '../../../../src/'
-  import { scaleLinear, scalePoint } from 'd3-scale'
-</script>
-
-<Graphic width={500} height={500}>
-
-</Graphic>
