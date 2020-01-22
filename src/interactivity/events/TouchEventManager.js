998c5723f1d2665ef84de73bba596bee57f59585
Ate Poorthuis <atepoorthuis@gmail.com>
2020-01-22 17:47:40 +0800

    fix: ssr rendering of site (#127)
    * chore: update gh token
    
    * fix: determine browser type only after mount
    
    needed to enable ssr
    
    * fix: add event manager type to constructor
    
    * chore: site export output to dev/null
    
    to prevent really large svelte css logs from clogging up netlify

--- src/interactivity/events/TouchEventManager.js
+++ src/interactivity/events/TouchEventManager.js
@@ -3,7 +3,26 @@ import capitalize from '../../utils/capitalize.js'
 
 export default class TouchEventManager extends BaseEventManager {
   constructor () {
-    super(EXPOSED_EVENTS)
+    super(EXPOSED_EVENTS, 'touch')
+  }
+
+  _getNativeTouchEventName (exposedEventName, BROWSER_TYPE) {
+    // In this mobile browser type, events are called 'pointerup' etc
+    if (BROWSER_TYPE === 'IE11 / MSEdge') {
+      const lastPart = sliceOffTouch(exposedEventName)
+      return 'pointer' + lastPart
+    }
+
+    // In this mobile browser type, events are called 'MSPointerUp' etc
+    if (BROWSER_TYPE === 'IE10 / WP8') {
+      const lastPart = sliceOffTouch(exposedEventName)
+      return 'MSPointer' + capitalize(lastPart)
+    }
+
+    // In other mobile browsers, events are called like the exposed ones
+    if (BROWSER_TYPE === 'other') {
+      return exposedEventName
+    }
   }
 
   _getScreenCoordinates (nativeEvent) {
@@ -36,39 +55,14 @@ export default class TouchEventManager extends BaseEventManager {
   }
 }
 
-const BROWSER_TYPE = window.navigator.pointerEnabled
-  ? 'IE11 / MSEdge'
-  : window.navigator.msPointerEnabled
-    ? 'IE10 / WP8'
-    : 'other'
-
 const EVENT_NAMES = ['touchstart', 'touchend', 'touchmove', 'touchcancel']
 
 const EXPOSED_EVENTS = EVENT_NAMES.map(eventName => ({
   eventName,
-  nativeEventName: getNativeTouchEventName(eventName),
+  nativeEventName: undefined,
   useWindow: false,
   preventDefault: true
 }))
-
-function getNativeTouchEventName (exposedEventName) {
-  // In this mobile browser type, events are called 'pointerup' etc
-  if (BROWSER_TYPE === 'IE11 / MSEdge') {
-    const lastPart = sliceOffTouch(exposedEventName)
-    return 'pointer' + lastPart
-  }
-
-  // In this mobile browser type, events are called 'MSPointerUp' etc
-  if (BROWSER_TYPE === 'IE10 / WP8') {
-    const lastPart = sliceOffTouch(exposedEventName)
-    return 'MSPointer' + capitalize(lastPart)
-  }
-
-  // In other mobile browsers, events are called like the exposed ones
-  if (BROWSER_TYPE === 'other') {
-    return exposedEventName
-  }
-}
 
 const sliceOffTouch = str => str.slice(5, str.length)
 
