e5907a887a0d5029b4e79ccff9535c5a396d7075
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-02-15 16:39:39 +0000

    feat: implemented transformGeometries and chainTransformations

--- src/utils/geometryUtils/transformGeometry/index.js
+++ src/utils/geometryUtils/transformGeometry/index.js
@@ -1,9 +1,63 @@
 export { transformGeometry } from './rendervous.esm.js'
 
-export function transformGeometries () {
+export function transformGeometries (geometries, transformation, settings) {
+  if (geometries.constructor === Array) {
+    const transformedGeometries = []
 
+    for (let i = 0; i < geometries.length; i++) {
+      transformedGeometries.push(transformation(
+        geometries[i],
+        transformation,
+        settings
+      ))
+    }
+
+    return transformedGeometries
+  }
+
+  if (geometries.constructor === Object) {
+    const transformedGeometries = {}
+
+    for (const key in geometries) {
+      transformedGeometries[key] = transformation(
+        geometries[key],
+        transformation,
+        settings
+      )
+    }
+
+    return transformedGeometries
+  }
 }
 
-export function chainTransformations () {
+export function chainTransformations ({ scale, coordinateTransformation, zoom }) {
+  const functions = []
 
+  if (scale) {
+    functions.push(scale)
+  }
+
+  if (coordinateTransformation) {
+    functions.push(coordinateTransformation)
+  }
+
+  if (zoom) {
+    functions.push(zoom)
+  }
+
+  return chainFunctions(functions)
+}
+
+function chainFunctions (functions) {
+  if (functions.length === 1) {
+    return functions[0]
+  }
+
+  if (functions.length === 2) {
+    return point => functions[1](functions[0](point))
+  }
+
+  if (functions.length === 3) {
+    return point => functions[2](functions[1](functions[0](point)))
+  }
 }
