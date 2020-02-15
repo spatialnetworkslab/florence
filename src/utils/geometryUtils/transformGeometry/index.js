ceaf2e9c3bc0831b9b1e02472b72b0f53f636e0d
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-02-15 18:33:51 +0000

    _

--- src/utils/geometryUtils/transformGeometry/index.js
+++ src/utils/geometryUtils/transformGeometry/index.js
@@ -44,35 +44,3 @@ export function transformGeometriesWithKeys (geometries, keys, transformation, s
 
   return transformedGeometries
 }
-
-export function chainTransformations ({ scale, coordinateTransformation, zoom }) {
-  const functions = []
-
-  if (scale) {
-    functions.push(scale)
-  }
-
-  if (coordinateTransformation) {
-    functions.push(coordinateTransformation)
-  }
-
-  if (zoom) {
-    functions.push(zoom)
-  }
-
-  return chainFunctions(functions)
-}
-
-function chainFunctions (functions) {
-  if (functions.length === 1) {
-    return functions[0]
-  }
-
-  if (functions.length === 2) {
-    return point => functions[1](functions[0](point))
-  }
-
-  if (functions.length === 3) {
-    return point => functions[2](functions[1](functions[0](point)))
-  }
-}
