1125a0498b3215a6cd6cf54e29c401cf4b2925f0
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-02-15 19:04:09 +0000

    _

--- src/components/Marks/Line/createPixelGeometryObject.js
+++ src/components/Marks/Line/createPixelGeometryObject.js
@@ -1,43 +1,43 @@
-import { createCoordSysGeometryObject } from '../utils/createCoordSysGeometry.js'
-import { scaleGeometries } from '../../../utils/geometryUtils'
-import {
-  ensureValidCombination, createScaledGeometryArrayFromXYProps
-} from '../utils/createScaledGeometryFromXYProps.js'
-import getKeyArray from '../utils/getKeyArray.js'
-import { isDefined, isUndefined } from '../../../utils/equals.js'
+// import { createCoordSysGeometryObject } from '../utils/createCoordSysGeometry.js'
+// import { scaleGeometries } from '../../../utils/geometryUtils'
+// import {
+//   ensureValidCombination, createScaledGeometryArrayFromXYProps
+// } from '../utils/createScaledGeometryFromXYProps.js'
+// import getKeyArray from '../utils/getKeyArray.js'
+// import { isDefined, isUndefined } from '../../../utils/equals.js'
 
-export default function (
-  geometryProps, sectionContext, coordinateTransformationContext, keyProp, interpolate
-) {
-  const { scaledGeometryArray, length } = createScaledGeometryArray(geometryProps, sectionContext)
-  const keyArray = getKeyArray(keyProp, length)
-  const coordSysGeometryObject = createCoordSysGeometryObject(
-    scaledGeometryArray, coordinateTransformationContext, keyArray, interpolate
-  )
+// export default function (
+//   geometryProps, sectionContext, coordinateTransformationContext, keyProp, interpolate
+// ) {
+//   const { scaledGeometryArray, length } = createScaledGeometryArray(geometryProps, sectionContext)
+//   const keyArray = getKeyArray(keyProp, length)
+//   const coordSysGeometryObject = createCoordSysGeometryObject(
+//     scaledGeometryArray, coordinateTransformationContext, keyArray, interpolate
+//   )
 
-  return coordSysGeometryObject
-}
+//   return coordSysGeometryObject
+// }
 
-function createScaledGeometryArray (geometryProps, sectionContext) {
-  ensureValidCombination(geometryProps)
-  if (isDefined(geometryProps.geometry)) {
-    if (geometryProps.geometry.constructor === Function) {
-      return geometryProps.geometry(sectionContext)
-    } else {
-      return scaleGeometryProp(geometryProps.geometry, sectionContext)
-    }
-  }
+// function createScaledGeometryArray (geometryProps, sectionContext) {
+//   ensureValidCombination(geometryProps)
+//   if (isDefined(geometryProps.geometry)) {
+//     if (geometryProps.geometry.constructor === Function) {
+//       return geometryProps.geometry(sectionContext)
+//     } else {
+//       return scaleGeometryProp(geometryProps.geometry, sectionContext)
+//     }
+//   }
 
-  if (isUndefined(geometryProps.geometry)) {
-    return createScaledGeometryArrayFromXYProps(
-      geometryProps.x, geometryProps.y, sectionContext, 'Line'
-    )
-  }
-}
+//   if (isUndefined(geometryProps.geometry)) {
+//     return createScaledGeometryArrayFromXYProps(
+//       geometryProps.x, geometryProps.y, sectionContext, 'Line'
+//     )
+//   }
+// }
 
-function scaleGeometryProp (geometry, scales) {
-  const scaledGeometryArray = scaleGeometries(geometry, scales)
-  const length = scaledGeometryArray.length
+// function scaleGeometryProp (geometry, scales) {
+//   const scaledGeometryArray = scaleGeometries(geometry, scales)
+//   const length = scaledGeometryArray.length
 
-  return { scaledGeometryArray, length }
-}
+//   return { scaledGeometryArray, length }
+// }
