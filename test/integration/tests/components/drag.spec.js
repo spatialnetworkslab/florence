f1212d4f1557b803aa57280afc6eea95a720101e
johsi-k <johsi.k@gmail.com>
2020-02-15 14:05:19 +0800

    fix: failing zoom and drag tests (#149)

--- test/integration/tests/components/drag.spec.js
+++ test/integration/tests/components/drag.spec.js
@@ -7,12 +7,22 @@ context('Assertions', () => {
 
   describe('Drag Tests', () => {
     it('[interaction] drag works', () => {
-      cy.get('circle.point')
-        .trigger('mousedown', { which: 1 })
-        .trigger('mousemove', { which: 1, clientX: 450, clientY: 450, force: true })
-        .trigger('mouseup')
-      cy.get('circle.point').invoke('attr', 'cx').should('be.greaterThan', 250)
-      cy.get('circle.point').invoke('attr', 'cy').should('be.greaterThan', 250)
+      cy.get('circle.point').then($point => {
+        const initcx = parseFloat($point.attr('cx'))
+        const initcy = parseFloat($point.attr('cy'))
+
+        cy.get('circle.point')
+          .trigger('mousedown', { which: 1 })
+          .trigger('mousemove', { which: 1, clientX: 450, clientY: 450, force: true })
+          .trigger('mouseup')
+          .then(() => {
+            const finalcx = parseFloat($point.attr('cx'))
+            const finalcy = parseFloat($point.attr('cy'))
+
+            expect(finalcx).to.not.equal(initcx)
+            expect(finalcy).to.not.equal(initcy)
+          })
+      })
     })
   })
 })
