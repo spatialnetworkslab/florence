af849fc6ddff6a56a209b0c8b4960276f0f6acf3
Ate Poorthuis <atepoorthuis@gmail.com>
2020-02-17 21:36:46 +0800

    feat: basic site structure (#150)
    * chore: replace sveltex with sveltex-unified
    
    `npm run build` with original sveltex
    ```
    real	0m29.716s
    user	0m58.563s
    sys	0m4.409s
    ```
    
    With sveltex-unified
    ```
    real	0m19.375s
    user	0m47.378s
    sys	0m2.849s
    ```
    
    Without repl
    ```
    real	0m13.832s
    user	0m31.788s
    sys	0m2.389s
    ```
    
    `npm run dev` startup time
    ```
    real	0m11.949s
    user	0m16.496s
    sys	0m1.491s
    ```
    
    * chore: switch site to npm version of florence
    
    * feat: init tailwind
    
    * feat: init structure for docs and gallery
    
    * feat: basic tailwind css styling
    
    * feat: add css to top-nav-bar and start work on side-nav-bar
    
    * feat: clean up side nav
    
    * fix: bring back top nav
    
    * feat: add col border
    
    * fix: fix top nav and side nav in position while scrolling
    
    * fix: clean up side nav and top nav
    
    * feat: clean up margins and padding
    
    * feat: fix padding
    
    * feat: update padding
    
    * feat: fix padding again
    
    * fix: remove non-existing tailwind classes
    
    * chore: lint
    
    * feat: collapsible sidenav
    
    * fix: remove nested screen directives
    
    these should be at the top level instead
    
    Co-authored-by: Ate Poorthuis <atepoorthuis@gmail.com>
    
    * fix: unknown prop class warnings in nav
    
    Co-authored-by: Bianchi Dy <44487900+bianchi-dy@users.noreply.github.com>
    Co-authored-by: johsi-k <johsi.k@gmail.com>

---
+++ site/tailwind.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      circle: 'circle'
+    },
+    extend: {
+      colors: {
+        codegray: '#f5f2f0'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
