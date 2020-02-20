263a9b3b0b7dc5a9fe0bc753f244955cfc9b5f93
Bianchi <bianchi_dy@sutd.edu.sg>
2020-02-20 15:41:19 +0800

    chore: lint categorical scales

--- site/src/routes/docs/guides/utils/_CategoricalScales.svelte
+++ site/src/routes/docs/guides/utils/_CategoricalScales.svelte
@@ -1,71 +1,18 @@
 <script>
   // d3
-  import { scaleBand, scaleThreshold, scaleDiverging, scaleSequential, scaleLinear, scalePow, scaleOrdinal, scaleSqrt, scaleLog } from 'd3-scale'
+  import { scaleBand, scaleLinear, scaleOrdinal} from 'd3-scale'
   import * as d3 from 'd3-scale-chromatic'
-  import { schemeCategory10, schemeAccent, schemeDark2, schemePaired, schemePastel1, schemePastel2, schemeSet1, schemeSet2, schemeSet3, interpolateHcl, rgb } from 'd3-scale-chromatic'
+  import { schemeAccent } from 'd3-scale-chromatic'
 
   // florence
-	import { Rectangle, Graphic, Grid, Section, PointLayer, Point, Label, LabelLayer, DiscreteLegend, GradientLegend, YAxis, XAxis } from '@snlab/florence'
+  import { Rectangle, Graphic, Section, DiscreteLegend, XAxis } from '@snlab/florence'
   import DataContainer from '@snlab/florence-datacontainer'
-
-	export let N = 100
-	const data = new DataContainer(generateData(N, 0.25))
-	function generateData (N, error) {
-		const getError = () => -error + (Math.random() * (2 * error)) * N
-		let data = { a: [], b: [] }
-		for (let i = 0; i < N; i++) {
-			data.a.push(i + getError())
-			data.b.push(i + getError())
-		}
-		return data
-  }
   
-  let threshold = 0
-  let filteredData
+  const height = 400
+  const width = 400
 
-  $: {
-    filteredData = data
-    .filter(row => row.a > threshold)
-  }
-
-	const scaleA = scaleLinear().domain(data.domain('a'))
-  const scaleB = scaleLinear().domain(data.domain('b'))
-  
-  let height = 400
-  let width = 400
-  let transformation = 'identity'
-  let duration = 2000
-
-  const log = console.log
-
-  let x = 0
-  let y = 0
-  let k = 1
-  let zoomIdentity = { x, y, kx: k, ky: k }
-
-  // scatterplot scale
-  const radiusScale = scaleLinear().domain(data.domain('b')).range([10, 0])
-
-  // scalar data
-  const bins = [[0, 30], [30, 70], [70, 100], [100, 155], [55, 300]]
-  const bins2 = [0, 10, 20, 40, 90, 120]
-  const fruits = ['apple', 'banana', 'orange', 'pomelo']
-
-  // fill scales
-  const linearColorScale = scaleLinear().domain(data.domain('a')).range(["red", "blue"])
-  const linearColorScaleBin = scaleLinear().domain(bins).range(["red", "blue"])
-  const seqScale = scaleSequential().domain(data.domain('a')).interpolator(d3.interpolateSinebow)
-  const linearColorScale2 =  scaleSequential().domain([0, 120]).interpolator(d3.interpolateViridis)
-  const fruitScale = scaleOrdinal().domain(fruits).range(schemeDark2)
-  const binScale = scaleLinear().domain([0, 4]).range(['red', 'blue'])
-
-  // fill opacity scales
-  const alphaScale = scaleLinear().domain(data.domain('a')).range([0, 1])
-  const fruitAlpha = scaleOrdinal().domain(fruits).range([0,1, 0.4, 0.2])
-  const binAlpha = scaleLinear().domain([0, 120]).range([0, 1])
-
- // categorical data
-   let catData = new DataContainer({
+  // categorical data
+  let catData = new DataContainer({
     fruit: ['apple', 'banana', 'apple', 'banana', 'apple', 'banana'],
     nutrient: ['carbs', 'carbs', 'fibre', 'fibre', 'protein', 'protein'],
     value: [3, 5, 1, 3, 4, 2]
@@ -81,7 +28,6 @@
     .select(['fruit', 'nutrient', 'valueFraction'])
     .groupBy('fruit')
 
-
   const containerPerFruit = catData.column('$grouped').map(container => {
     return container.cumsum({ cumsum_value: 'valueFraction' })
   })
@@ -89,7 +35,6 @@
   const nutrientColorScale = scaleOrdinal()
     .domain(nutrientDomain)
     .range(schemeAccent)
-
 </script>
 
 <div>
