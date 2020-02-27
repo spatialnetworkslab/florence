ffff23181e4bcaf7b468bf4e2702831e679a1df6
Bianchi <bianchi_dy@sutd.edu.sg>
2020-02-27 18:35:56 +0800

    chore: update legends tables descriptions

--- site/src/routes/docs/guides/utils/_UsePadding.svelte
+++ site/src/routes/docs/guides/utils/_UsePadding.svelte
@@ -3,7 +3,7 @@
   import { scaleLinear } from 'd3-scale'
 
   // florence
-  import { Graphic, Section, PointLayer, DiscreteLegend, GradientLegend, YAxis, XAxis } from '@snlab/florence'
+  import { Graphic, Section, DiscreteLegend, GradientLegend, YAxis, XAxis } from '@snlab/florence'
   import DataContainer from '@snlab/florence-datacontainer'
 
   export let N = 100
@@ -20,51 +20,49 @@
 
   const data = new DataContainer(generateData(N, 0.25))
   const threshold = 0
-  let filteredData
+  let filteredData = undefined
 
   $: {
     filteredData = data
       .filter(row => row.a > threshold)
   }
 
-  const height = 400
-  const width = 400
-  const x = 0
-  const y = 0
-  const k = 1
-  const zoomIdentity = { x, y, kx: k, ky: k }
-
   // fill scales
   const linearColorScale = scaleLinear().domain(data.domain('a')).range(['red', 'blue'])
+  const sectionPadding = 75
+  const graphicPadding = 50
+  const background = '#a8a8a8'
+  const padding = '#E8E8E8'
 </script>
 
 <div>
-	<Graphic 
-    {width} {height}
-  >         
+  <Graphic width={500} height={500}
+    padding={graphicPadding}
+  >     
 
     <!-- Basic example + continuous scales -->
     <Section 
-      x1={0} x2={400}
-      y1={0} y2={400}
-      padding={30}
+      x1={0} x2={500}
+      y1={0} y2={500}
+      padding={sectionPadding}
       scaleX={scaleLinear().domain(data.domain('a'))}
       scaleY={scaleLinear().domain(data.domain('b'))}
-      {zoomIdentity}
+      backgroundColor={background}
+      paddingColor={padding} 
     >
-      <!-- NOTE: usePadding won't work on the first two legend examples here
-      because they are being given specific values -->
 
       <!-- Vjust -->
-      <DiscreteLegend
+      <GradientLegend
+        title={'Gradient'}
         fill={linearColorScale}
         labelCount={5}
         flip
-        usePadding={true}
+        usePadding={false}
       />
 
       <!-- Pixels -->
-      <GradientLegend
+      <DiscreteLegend
+        title={'Discrete'}
         x1={() => { return 200 }} x2={() => { return 300 }}
         y1={() => { return 60 }} y2={() => { return 100 }}
         fill={linearColorScale}
@@ -75,15 +73,8 @@
         usePadding={true}
       />
       
-      <PointLayer
-          x={filteredData.column('a')}
-          y={filteredData.column('b')}
-          key={filteredData.column('$key')}
-          fill={linearColorScale}
-        />
-
-      <XAxis zoomIdentity={{ y: 0, ky: 1 }} />
-      <YAxis zoomIdentity={{ x: 0, kx: 1 }} />
+      <XAxis />
+      <YAxis />
     
     </Section>
 
