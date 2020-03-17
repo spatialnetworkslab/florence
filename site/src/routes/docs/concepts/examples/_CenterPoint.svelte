915e5a5259110910ed9cf36a115b0df009668cb1
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-03-17 16:48:48 +0000

    docs: more work on local coordinates

---
+++ site/src/routes/docs/concepts/examples/_CenterPoint.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Graphic, Point } from '@snlab/florence'
+  import { scaleLinear } from 'd3-scale'
+
+  const point = { x: 5, y: 5 }
+</script>
+
+<Graphic
+  width={500}
+  height={500}
+  scaleX={scaleLinear().domain([0, 10])}
+  scaleY={scaleLinear().domain([0, 10])}
+>
+
+  <Point x={point.x} y={point.y} radius={10} fill="red" />
+
+</Graphic>
