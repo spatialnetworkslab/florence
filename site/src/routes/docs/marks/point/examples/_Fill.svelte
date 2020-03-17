fa81e0aebad42516779039bd589048e4bdc43e32
dave <david_tseng1992@hotmail.com>
2020-03-17 15:30:29 +0800

    feat: better example

--- site/src/routes/docs/marks/point/examples/_Fill.svelte
+++ site/src/routes/docs/marks/point/examples/_Fill.svelte
@@ -1,34 +1,35 @@
 <script>
-import { XAxis, YAxis, Graphic, PointLayer, Title, Section } from '@snlab/florence'
-import { schemeCategory10 } from 'd3-scale-chromatic'
-import { scaleLinear } from 'd3-scale'
-const WIDTH = 500
-const HEIGHT = 200
-const flameTemparatureGradient = [20, 200, 600, 800, 1000, 1200, 1400]
+  import {
+    XAxis,
+    YAxis,
+    Graphic,
+    PointLayer,
+    Title,
+    Section
+  } from '@snlab/florence'
+  import { schemeCategory10 } from 'd3-scale-chromatic'
+  import { scaleLinear } from 'd3-scale'
+  const WIDTH = 500
+  const HEIGHT = 200
+  const flameTemparatureGradient = [20, 200, 600, 800, 1000, 1200, 1400]
 
-var x = scaleLinear()
-  .domain([0, 180])
-  .range([0, WIDTH])
-
-var y = scaleLinear()
-  .domain([0, 100])
-  .range([0, HEIGHT])
-
-var fill = scaleLinear()
-  .domain([20, 1400])
-  .range(schemeCategory10)
+  var fillScale = scaleLinear()
+    .domain([20, 1400])
+    .range(schemeCategory10)
 </script>
 
-<Graphic width={WIDTH} height={HEIGHT} >
-    <Title title={'Flame Temparature Gradient'} titleFontSize={'12'}>
-    </Title>
-    <Section flipY padding={{ top: 10, bottom: 30, left: 40, right: 10 }}>
-        <PointLayer 
-            x={[20, 40, 60, 80, 100, 120, 140].map(d => x(d))} 
-            y={[50, 50, 50, 50, 50, 50, 50].map(d => y(d))} 
-            fill={flameTemparatureGradient.map(d => fill(d))} 
-            radius={4}/>
-            <XAxis/>
-            <YAxis/>
-    </Section>
-</Graphic> 
+<Graphic width={WIDTH} height={HEIGHT}>
+  <Title title={'Flame Temparature Gradient'} titleFontSize={'12'} />
+  <Section
+    scaleX={scaleLinear().domain([0, 180])}
+    scaleY={scaleLinear().domain([0, 100])}
+    flipY
+    padding={{ top: 10, bottom: 30, left: 40, right: 10 }}>
+    <PointLayer
+      x={[20, 40, 60, 80, 100, 120, 140]}
+      y={[50, 50, 50, 50, 50, 50, 50]}
+      fill={flameTemparatureGradient.map(fillScale)} />
+    <XAxis />
+    <YAxis />
+  </Section>
+</Graphic>
--- site/src/routes/docs/marks/point/examples/_Radius.svelte
+++ site/src/routes/docs/marks/point/examples/_Radius.svelte
@@ -5,11 +5,10 @@
     Graphic,
     PointLayer,
     Title,
-    Section,
-    LabelLayer
+    Section
   } from '@snlab/florence'
 
-  import { scaleLinear, scaleBand } from 'd3-scale'
+  import { scaleLinear, scalePoint } from 'd3-scale'
   const WIDTH = 500
   const HEIGHT = 200
   const planetName = [
@@ -24,35 +23,23 @@
   ]
   const planetRadius = [2.44, 6.05, 6.37, 3.39, 70, 58.2, 25.4, 24.6]
 
-  const planetScale = scaleBand()
-    .domain(planetName)
-    .range([0, WIDTH])
-  const rRadius = scaleLinear()
+  const scaleRadius = scaleLinear()
     .domain([Math.min(...planetRadius), Math.max(...planetRadius)])
     .range([0, 30])
-
-  var y = scaleLinear()
-    .domain([0, 100])
-    .range([0, HEIGHT])
 </script>
 
 <Graphic width={WIDTH} height={HEIGHT}>
   <Title title={'Solar system radius'} titleFontSize={'12'} />
-  <Section flipY padding={{ top: 10, bottom: 30, left: 40, right: 10 }}>
-    <LabelLayer
-      text={planetName}
-      x={planetName.map(planetScale)}
-      y={[80, 80, 80, 80, 80, 80, 80, 80].map(y)} />
+  <Section
+    scaleX={scalePoint().domain(planetName)}
+    scaleY={scaleLinear().domain([0, 100])}
+    flipY
+    padding={{ top: 10, bottom: 30, left: 40, right: 10 }}>
     <PointLayer
-      x={planetName.map(planetScale)}
-      y={[50, 50, 50, 50, 50, 50, 50, 50].map(y)}
+      x={planetName}
+      y={[50, 50, 50, 50, 50, 50, 50, 50]}
       fill={'#334d00'}
-      radius={planetRadius.map(rRadius)} />
-    <!-- <PointLayer 
-            x={[20, 40, 60, 80, 100, 120, 140].map(d => x(d))} 
-            y={[50, 50, 50, 50, 50, 50, 50].map(d => y(d))} 
-            fill={flameTemparatureGradient.map(d => fill(d))} 
-            radius={4}/> -->
+      radius={planetRadius.map(scaleRadius)} />
     <XAxis />
     <YAxis />
   </Section>
