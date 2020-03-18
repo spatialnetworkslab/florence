1d13a4570bcb81c04a057fc01804ac61d82778a1
Ate Poorthuis <atepoorthuis@gmail.com>
2020-03-18 20:45:13 +0800

    feat: simplify point docs

--- site/src/routes/docs/marks/point/examples/_Simple.svelte
+++ site/src/routes/docs/marks/point/examples/_Simple.svelte
@@ -7,27 +7,25 @@
     Title,
     Section
   } from '@snlab/florence'
-  //   import { schemeCategory10 } from 'd3-scale-chromatic'
   import { scaleLinear, scaleTime } from 'd3-scale'
-  const WIDTH = 500
-  const HEIGHT = 200
+  const width = 200
+  const height = 200
   const x = [
-    new Date(2000, 0, 1),
-    new Date(2005, 0, 1),
-    new Date(2010, 0, 1),
-    new Date(2015, 0, 1)
+    new Date(2002, 0, 1),
+    new Date(2004, 0, 1),
+    new Date(2006, 0, 1)
   ]
-  const y = [100, 110, 130, 180]
+  const y = [20, 40, 10]
 </script>
 
-<Graphic width={WIDTH} height={HEIGHT}>
-  <Title title={'Simple'} titleFontSize={'12'} />
+<Graphic {width} {height}>
   <Section
-    scaleX={scaleTime().domain([new Date(2000, 0, 1), new Date(2016, 0, 1)])}
+    scaleX={scaleTime().domain([new Date(2001, 0, 1), new Date(2007, 0, 1)])}
     scaleY={scaleLinear().domain([0, Math.max(...y)])}
     flipY
-    padding={{ top: 10, bottom: 30, left: 40, right: 10 }}>
-    <PointLayer {x} {y} radius={4} />
+    padding={20}
+  >
+    <PointLayer {x} {y} />
     <XAxis />
     <YAxis />
   </Section>
