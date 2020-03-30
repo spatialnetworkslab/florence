b9f9735e4a1cef335adefcc987f26a7e4a0040bc
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-03-30 14:23:27 +0200

    feat: zoomIdentity scaling factors must be positive

--- src/components/Core/utils/padding.js
+++ src/components/Core/utils/padding.js
@@ -23,6 +23,8 @@ export function parsePadding (_padding) {
 const invalidPaddingError = new Error('Invalid padding specification')
 
 export function applyPadding (range, offsetMin, offsetMax) {
+  ensurePaddingSmallerThanRange(range, offsetMin, offsetMax)
+
   if (range[0] < range[1]) {
     return [range[0] + offsetMin, range[1] - offsetMax]
   } else {
@@ -35,5 +37,11 @@ export function removePadding (range, offsetMin, offsetMax) {
     return [range[0] - offsetMin, range[1] + offsetMax]
   } else {
     return [range[0] + offsetMax, range[1] - offsetMin]
+  }
+}
+
+function ensurePaddingSmallerThanRange (range, min, max) {
+  if (Math.abs(range[0] - range[1]) < (min + max)) {
+    console.warn('Padding cannot exceed width or height')
   }
 }
