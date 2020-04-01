4e7e2d7c82acb007a4a73428a6b3a22474d3d554
Ate Poorthuis <atepoorthuis@gmail.com>
2020-04-01 23:11:16 +0800

    feat: edits to choropleth tutorial

---
+++ site/sandboxes/intermediate-tutorial-init/App.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { Graphic, Section, PolygonLayer, createGeoScales, Title } from '@snlab/florence'
+  import DataContainer from '@snlab/florence-datacontainer'
+  import { scaleThreshold } from 'd3-scale'
+  import { geodata } from './planning_areas_data.js'
+</script>
