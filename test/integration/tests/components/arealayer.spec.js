b45f3cdbf7142f6126d14272af2560e234265769
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-17 22:17:09 +0100

    enabled integration tests again, but removed snapshot stuff

--- test/integration/tests/components/arealayer.spec.js
+++ test/integration/tests/components/arealayer.spec.js
@@ -2,20 +2,20 @@
 
 context('Assertions', () => {
   beforeEach(() => {
-    cy.visit('/area')
+    cy.visit('/arealayer')
   })
 
-  describe('Area Tests', () => {
-    it('[render] area rendered', () => {
-      cy.get('.area').should('have.length', 1)
+  describe('AreaLayer Tests', () => {
+    it('[render] area layer rendered', () => {
+      cy.get('.area').should('have.length', 2)
     })
 
-    it('[render] area snapshot correctly', () => {
-      cy.get('svg').toMatchSnapshot()
-    })
+    // it('[render] area layer snapshot correctly', () => {
+    //   cy.get('svg').toMatchSnapshot()
+    // })
 
-    it('[interaction] hovering over area changes color', () => {
-      cy.get('.area').then(($area) => {
+    it('[interaction] hovering over area layer changes color', () => {
+      cy.get('.area').first().then(($area) => {
         const originalFill = $area.attr('fill')
         cy.wrap($area).trigger('mousemove').its('fill').should('not.be', originalFill)
         cy.get('.left-col').trigger('mousemove') // unhovers by moving cursor to sidebar
