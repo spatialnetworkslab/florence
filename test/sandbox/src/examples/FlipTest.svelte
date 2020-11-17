480f8d3b932b346eb81fa817e3c7781f965eca5a
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-17 20:10:56 +0100

    test: axis bug (not able to reproduce so far)

--- test/sandbox/src/examples/FlipTest.svelte
+++ test/sandbox/src/examples/FlipTest.svelte
@@ -1,9 +1,19 @@
 <script>
-  import { Graphic, Section, Point } from '../../../../src'
+  import { Graphic, Section, Point, XAxis, YAxis } from '../../../../src'
 </script>
 
-<Graphic width={200} height={200} flipY flipX>
+<Graphic 
+  width={500}
+  height={500}
+  flipX={false}
+  flipY={false}
+  padding={{ left: 50, right: 50, top: 50, bottom: 20 }}
+  paddingColor="blue"
+>
 
   <Point x={10} y={10} />
+
+  <XAxis />
+  <YAxis />
 
 </Graphic>
