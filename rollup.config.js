7d7ce86d74e0dd2ad51b7c5271f0d8912b636003
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-23 13:07:54 +0100

    feat: expose esm module

--- rollup.config.js
+++ rollup.config.js
@@ -9,36 +9,70 @@ import pkg from './package.json';
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-	input: 'src/index.js',
-	output: [
-		{ file: pkg.module, 'format': 'es' }
-		// { file: pkg.main, 'format': 'umd', name: 'florence' }
-	],
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production
-		}),
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.module, 'format': 'es' }
+    ],
+    plugins: [
+      svelte({
+        // enable run-time checks when not in production
+        dev: !production
+      }),
+  
+      // If you have external dependencies installed from
+      // npm, you'll most likely need these plugins. In
+      // some cases you'll need additional configuration —
+      // consult the documentation for details:
+      // https://github.com/rollup/rollup-plugin-commonjs
+      resolve(),
+      commonjs(),
+      // analyze({ summaryOnly: true }),
+  
+      // Allows you to use 'process'
+      globals(),
+  
+      // Watch the `public` directory and refresh the
+      // browser on changes when not in production
+      !production && livereload('public'),
+  
+      // If we're building for production (npm run build
+      // instead of npm run dev), minify
+      production && terser()
+    ]
+  },
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration —
-		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
-		resolve(),
-		commonjs(),
-		// analyze({ summaryOnly: true }),
-
-		// Allows you to use 'process'
-		globals(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	]
-};
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.svelte, 'format': 'esm', name: 'florence' }
+    ],
+    plugins: [
+      svelte({
+        // enable run-time checks when not in production
+        dev: !production
+      }),
+  
+      // If you have external dependencies installed from
+      // npm, you'll most likely need these plugins. In
+      // some cases you'll need additional configuration —
+      // consult the documentation for details:
+      // https://github.com/rollup/rollup-plugin-commonjs
+      resolve(),
+      commonjs(),
+      // analyze({ summaryOnly: true }),
+  
+      // Allows you to use 'process'
+      globals(),
+  
+      // Watch the `public` directory and refresh the
+      // browser on changes when not in production
+      !production && livereload('public'),
+  
+      // If we're building for production (npm run build
+      // instead of npm run dev), minify
+      production && terser()
+    ]
+  }
+]
