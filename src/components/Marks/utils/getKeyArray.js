b57ad07cadf0de4609bf410f36d9a7f6c2c0ec4f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-24 10:47:02 +0100

    fix: keys now have to be strings

--- src/components/Marks/utils/getKeyArray.js
+++ src/components/Marks/utils/getKeyArray.js
@@ -2,9 +2,12 @@ export default function (keyProp, length) {
   if (keyProp) {
     if (keyProp.constructor !== Array) throw new Error('\'key\' must be Array')
     if (keyProp.length !== length) throw new Error('\'key\' must be of same length as positioning props')
+    if (keyProp.some(key => key.constructor !== String)) {
+      throw new Error('Keys can only be Strings')
+    }
 
     return keyProp
   } else {
-    return new Array(length).fill(0).map((_, i) => i)
+    return new Array(length).fill(0).map((_, i) => i.toString())
   }
 }
