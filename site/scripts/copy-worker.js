9d5b585662ef7ab55a8e1441c5d51d36bc45729a
Ate Poorthuis <atepoorthuis@gmail.com>
2020-11-24 21:16:53 +0100

    fix: create static/workers dir if not exists

--- site/scripts/copy-worker.js
+++ site/scripts/copy-worker.js
@@ -1,5 +1,6 @@
 const sh = require('shelljs')
 
+sh.mkdir('-p', 'static/workers/')
 sh.rm('-rf', 'static/workers/bundler.js')
 sh.rm('-rf', 'static/workers/bundler.js.map')
 sh.cp('-r', 'node_modules/@snlab/florence-repl/public/workers/bundler.js', 'static/workers/bundler.js')
