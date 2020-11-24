f1992b8351e02474aba4badd19be9e53cbe398c2
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-24 14:42:39 +0100

    _

--- site/scripts/copy-worker.js
+++ site/scripts/copy-worker.js
@@ -1,6 +1,6 @@
 const sh = require('shelljs')
 
-sh.rm('-rf', 'static/bundler.js')
-sh.rm('-rf', 'static/bundler.js.map')
-sh.cp('-r', 'node_modules/@snlab/florence-repl/public/bundler.js', 'static')
-sh.cp('-r', 'node_modules/@snlab/florence-repl/public/bundler.js.map', 'static')
+sh.rm('-rf', 'static/workers/bundler.js')
+sh.rm('-rf', 'static/workers/bundler.js.map')
+sh.cp('-r', 'node_modules/@snlab/florence-repl/public/workers/bundler.js', 'static/workers')
+sh.cp('-r', 'node_modules/@snlab/florence-repl/public/workers/bundler.js.map', 'static/workers')
