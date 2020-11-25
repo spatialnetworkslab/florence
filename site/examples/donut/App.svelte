2695f5f8b0019260533709b5150609d9752620ba
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-25 16:01:16 +0100

    docs: refactor donut example

--- site/examples/donut/App.svelte
+++ site/examples/donut/App.svelte
@@ -1,90 +1,82 @@
-<!-- adapted from https://observablehq.com/@d3/donut-chart -->
 <script>
-  import { Graphic, Section, RectangleLayer, LabelLayer } from '@snlab/florence';
-  import DataContainer from "@snlab/florence-datacontainer";
-  import { scaleLinear, scaleOrdinal } from "d3-scale";
-  import { quantize } from "d3-interpolate";
-  import { interpolateSpectral } from "d3-scale-chromatic";
-  import { csv } from "d3-fetch";
-  import { autoType } from "d3-dsv";
+  import { scaleOrdinal } from 'd3-scale'
+  import { quantize } from 'd3-interpolate'
+  import { interpolateSpectral } from 'd3-scale-chromatic'
+  import { csv } from 'd3-fetch'
+  import { autoType } from 'd3-dsv'
+  import { Graphic, RectangleLayer, LabelLayer } from '@snlab/florence'
+  import DataContainer from '@snlab/florence-datacontainer'
 
-  let done = false;
-  let parsedData;
-  const data = csv(
-    "/data/population.csv",
-    // "https://gist.githubusercontent.com/mbostock/a3541c73fdccd432acc8b11bf9f02641/raw/2bd0fce0bf34b020e93c5f6527b5a9d08c33ff06/population-by-age.csv",
-    autoType
-  ).then(d => {
-    parsedData = d;
-    done = true;
-  });
+  let dataContainer, nrow, scaleColor
 
-  let dataContainer, cumSum;
-  let scaleX, scaleY, scaleColor;
-  let numRows
-  $: {
-    if (done) {
-      dataContainer = new DataContainer(parsedData);
-      cumSum = dataContainer.cumsum({ cumsum: "value" }, { asInterval: true });
-      numRows = cumSum.column('cumsum').length
-      scaleX = scaleLinear().domain([0, cumSum.domain("cumsum")[1]]);
-      scaleY = scaleLinear().domain([0, 15]);
-      scaleColor = scaleOrdinal()
-        .domain(cumSum.domain("name"))
-        .range(
-          quantize(
-            t => interpolateSpectral(t * 0.8 + 0.1),
-            cumSum.rows().length
-          ).reverse()
-        );
-    }
-  }
-</script> 
+  (async () => {
+    const data = await csv('/data/population.csv', autoType)
 
-<Graphic width={500} height={530}>
-  {#if done}
-    <Section
-      {scaleX}
-      {scaleY}
-      transformation={'polar'}
-      flipY
-    >
-      <!-- slices and age labels -->
-      <RectangleLayer 
-        x1={cumSum.map('cumsum', d => d[0])}
-        x2={cumSum.map('cumsum', d => d[1])}
-        y1={Array(numRows).fill(10)}
-        y2={Array(numRows).fill(15)}
-        fill={cumSum.map('name', scaleColor)}
-        stroke={'white'}
-        strokeWidth={1}
-      />
-      <LabelLayer
-        x={cumSum.map('cumsum', d => d[0] + (d[1] - d[0]) / 2)}
-        y={Array(numRows).fill(12.5)}
-        text={cumSum.column('name')}
-        fontSize={11}
-        fontWeight={'bold'}
-        fontFamily={'sans-serif'}
-      />
-    </Section>
+    dataContainer = new DataContainer(data)
+      .rename({ value: 'population', name: 'age_group' })
+      .cumsum(
+        { cumulativePopulation: 'population' },
+        { asInterval: true }
+      ).mutate({
+        lower: row => row.cumulativePopulation[0],
+        upper: row => row.cumulativePopulation[1],
+        mid: ({ lower, upper }) => lower + (upper - lower) / 2
+      })
 
-    <!-- population labels (omitted in last 4 slices) -->
-    <Section
-      {scaleX}
-      {scaleY}
-      transformation={'polar'}
-      flipY
-      zoomIdentity={{ x: 0, y: 15, kx: 1, ky: 1 }}
-    >
-      <LabelLayer
-        x={cumSum.column('cumsum').slice(0, numRows - 4).map(d => d[0] + (d[1] - d[0]) / 2)}
-        y={Array(numRows - 4).fill(12.5)}
-        text={cumSum.column('value').slice(0, numRows - 4).map(d => d.toLocaleString())}
-        fontSize={11}
-        fontWeight={'lighter'}
-        fontFamily={'sans-serif'}
-      />
-    </Section>
-  {/if}
-</Graphic>
+    nrow = dataContainer.nrow()
+
+    const colors = quantize(
+      t => interpolateSpectral(t * 0.8 + 0.1),
+      dataContainer.nrow()
+    ).reverse()
+
+    scaleColor = scaleOrdinal()
+      .domain(cumSum.domain('age_group'))
+      .range(colors)
+  })()
+</script>
+
+{#if dataContainer}
+
+  <Graphic
+    width={500}
+    height={500}
+    scaleX={[0, dataContainer.max('cumulativePopulation')]}
+    scaleY={[0, 15]}
+    transformation={'polar'}
+    flipY
+  >
+
+    <RectangleLayer 
+      x1={dataContainer.column('lower')}
+      x2={dataContainer.column('upper')}
+      y1={Array(nrow).fill(10)}
+      y2={Array(nrow).fill(15)}
+      fill={dataContainer.map('age_group', scaleColor)}
+      stroke={'white'}
+      strokeWidth={1}
+    />
+  
+    <!-- age groups -->
+    <LabelLayer
+      x={dataContainer.column('mid')}
+      y={Array(nrow).fill(12.5)}
+      text={dataContainer.column('age_group')}
+      fontSize={11}
+      fontWeight={'bold'}
+      fontFamily={'sans-serif'}
+    />
+
+    <!-- population labels, except last four -->
+    <LabelLayer
+      x={dataContainer.column('cumulativePopulation').slice(0, nrow - 4)}
+      y={Array(nrow - 4).fill(12.5)}
+      text={dataContainer.column('population').slice(0, nrow - 4).map(d => d.toLocaleString())}
+      fontSize={11}
+      fontWeight={'lighter'}
+      fontFamily={'sans-serif'}
+    />
+
+  </Graphic>
+
+{/if}
