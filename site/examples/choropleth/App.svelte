f9d939704e601fc5851ff27ec5315cc150dded39
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-25 13:39:29 +0100

    docs: refactor choropleth example

--- site/examples/choropleth/App.svelte
+++ site/examples/choropleth/App.svelte
@@ -1,33 +1,28 @@
 <script>
-  import { Graphic, PolygonLayer, createGeoScales } from '@snlab/florence'
-  import DataContainer from '@snlab/florence-datacontainer'
   import { json, csv } from 'd3-fetch'
   import { feature } from 'topojson'
-  import { scaleQuantize } from 'd3-scale'
   import { schemeBlues } from 'd3-scale-chromatic'
+  import { Graphic, PolygonLayer, createGeoScales } from '@snlab/florence'
+  import DataContainer from '@snlab/florence-datacontainer'
 
-  let geometries
-  let geoScales
+  let geometries, geoScales, populationScale
+  
+  (async () => {
+    const topojson = await json('/data/us-states.topojson')
+    geometries = new DataContainer(feature(topojson, topojson.objects.states))
+    geoScales = createGeoScales(geometries.bbox())
 
-  json('/data/us-states.topojson').then(topojson => {
-    const features = feature(topojson, topojson.objects.states)
-    geometries = new DataContainer(features)
-    geoScales = createGeoScales(geometries.domain('$geometry'))
-  })
+    const population = await csv('/data/us-states-population.csv')
+    geometries.addColumn('population', population.map(row => +row.population))
 
-  let population
-  let populationScale = scaleQuantize()
-    .domain([0, 40000000])
-    .range(schemeBlues[9])
-
-  csv('/data/us-states-population.csv').then(csv => {
-    population = csv.map(row => +row.population)
-  })
-
-  $: done = geometries && population
+    populationScale = geometries.classify({
+      column: 'population', method: 'Jenks', numClasses: 6
+    }, schemeBlues[6])
+  })()
 </script>
 
-{#if done}
+{#if geometries}
+
   <Graphic 
     width={500}
     height={500}
@@ -36,8 +31,8 @@
 
     <PolygonLayer 
       geometry={geometries.column('$geometry')}
-      fill={population.map(populationScale)}
-      stroke="grey"
+      fill={geometries.map('population', populationScale)}
+      stroke={'grey'}
       strokeWidth={0.5}
     />
 
