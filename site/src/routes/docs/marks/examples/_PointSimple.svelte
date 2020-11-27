160a4525fc9fb5dd0ae44a929e6edb5a93d6c505
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-27 18:34:46 +0100

    docs: rewrote point doc

--- site/src/routes/docs/marks/examples/_PointSimple.svelte
+++ site/src/routes/docs/marks/examples/_PointSimple.svelte
@@ -5,8 +5,7 @@
 <Graphic width={200} height={200} scaleX={[0, 10]} scaleY={[0, 10]} padding={20}>
 
   <Point x={2.5} y={2.5} fill={'red'} />
-
-  <PointLayer x={[5, 7.5]} y={[5, 7.5]} fill={'blue'} />
+  <PointLayer x={[5, 7.5]} y={[5, 7.5]} fill={['blue', 'green']} />
 
   <XAxis />
   <YAxis />
