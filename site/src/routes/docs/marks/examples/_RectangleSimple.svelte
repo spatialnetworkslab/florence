da714f22ec7b2d67f70e69589b2cc9d8bf2c52f3
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-28 16:57:17 +0100

    docs: rewrote rectangle docs

--- site/src/routes/docs/marks/examples/_RectangleSimple.svelte
+++ site/src/routes/docs/marks/examples/_RectangleSimple.svelte
@@ -1,36 +1,32 @@
 <script>
-  import {
-    XAxis,
-    YAxis,
-    Graphic,
-    RectangleLayer,
-    Section
-  } from '@snlab/florence'
+  import { Graphic, Rectangle, RectangleLayer, XAxis, YAxis } from '@snlab/florence'
   import { scaleLinear, scaleBand } from 'd3-scale'
-  const width = 200
-  const height = 200
+
   const x = [2002, 2004, 2006]
   const y = [20, 40, 10]
-  const scaleX = scaleBand()
-    .domain(x)
-    .padding(0.5)
-  const scaleY = scaleLinear()
-    .domain([0, Math.max(...y)])
+
+  const scaleX = scaleBand().domain(x).padding(0.5)
+  const scaleY = scaleLinear().domain([0, Math.max(...y)])
 </script>
 
-<Graphic {width} {height}>
-  <Section
-    {scaleX}
-    {scaleY}
-    flipY
-    padding={20}
-  >
-     <RectangleLayer
-      x1={x}
-      x2={({ scaleX }) => x.map(value => scaleX(value) + scaleX.bandwidth())}
-      y1={y}
-      y2={0} />
-    <XAxis />
-    <YAxis />
-  </Section>
+<Graphic width={200} height={200} {scaleX} {scaleY} flipY padding={20}>
+
+  <Rectangle 
+    x1={2002}
+    x2={({ scaleX }) => scaleX(2006) + scaleX.bandwidth()}
+    y1={0}
+    y2={20}
+    opacity={0.3}
+  />
+
+  <RectangleLayer
+    x1={x}
+    x2={({ scaleX }) => x.map(value => scaleX(value) + scaleX.bandwidth())}
+    y1={0}
+    y2={y}
+  />
+  
+  <XAxis />
+  <YAxis />
+  
 </Graphic>
---
+++ site/src/routes/docs/marks/utils/_RectangleInherit.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Graphic, Rectangle, XAxis, YAxis } from '@snlab/florence'
+</script>
+
+<Graphic width={200} height={200} scaleX={[0, 10]} scaleY={[0, 10]} padding={20}>
+  
+  <Rectangle fill={'blue'} />
+  <Rectangle x1={4} x2={6} fill={'red'} />
+  <Rectangle y1={4} y2={6} fill={'green'} />
+
+  <XAxis />
+  <YAxis />
+
+</Graphic>
