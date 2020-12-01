b00fda2df170d5dbc3f9b23115fcf27788360c1b
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-12-01 14:07:52 +0100

    docs: candlestick (removed semicolons)

--- site/examples/candlestick/App.svelte
+++ site/examples/candlestick/App.svelte
@@ -1,70 +1,93 @@
 <script>
   import { csv } from 'd3-fetch'
   import { format } from 'd3-format'
-  import { autoType } from 'd3-dsv'
   import { timeDay, timeMonday } from 'd3-time'
-  import { timeFormat } from 'd3-time-format'
-  import { scaleLog, scaleBand } from 'd3-scale'
-  import { Graphic, LineLayer, XAxis, YAxis } from '@snlab/florence'
+  import { timeParse, timeFormat } from 'd3-time-format'
+  import { scaleLinear, scaleLog, scaleBand } from 'd3-scale'
+  import { Graphic, Section, LineLayer, XAxis, YAxis } from '@snlab/florence'
   import DataContainer from '@snlab/florence-datacontainer'
 
-  let appleStockData, scaleX, scaleY, ticksX, ready
+  const parseDate = timeParse('%Y-%m-%d')
 
-  (async () => {
-    const data = await csv('/data/apple-stocks-candlestick.csv', autoType)
-      .slice(-120)
-    
-    appleStockData = new DataContainer(data)
-      .mutate({
-        dates: row => [row.Date, row.Date],
-        lowHigh: row => [row.Low, row.High],
-        openClose: row => [row.Open, row.Close]
-      })
+  let done = false
+  let data
+  csv('/data/apple-stocks-candlestick.csv', d => {
+    const date = parseDate(d['Date'])
+    return {
+      date,
+      high: +d['High'],
+      low: +d['Low'],
+      open: +d['Open'],
+      close: +d['Close']
+    }
+  }).then(d => {
+    data = d.slice(-120)
+    done = true
+  })
 
-    const noWeekend = d => d.getDay() !== 0 && d.getDay() !== 6
+  const padding = { top: 20, bottom: 30, left: 40, right: 30 }
+  const width = 800
+  const height = 600
 
-    scaleX = scaleBand().padding(0.2).domain(
-      timeDay().range(appleStockData.domain('date')).filter(noWeekend)
-    )
+  let dataContainer
+  let domainHigh, domainLow, domainDate
+  let scaleX, scaleY
+  let xTicks, scaleYAxis
 
-    scaleY = scaleLog().domain([
-      appleStockData.min('Low'),
-      appleStockData.max('High')
-    ])
+  $: {
+    if (done) {
+      dataContainer = new DataContainer(data)
 
-    ticksX = timeMonday.every(1).range(domainDate[0], domainDate[1])
-    ready = true
-  })()
+      domainHigh = dataContainer.domain('high')
+      domainLow = dataContainer.domain('low')
+      domainDate = dataContainer.domain('date')
 
-  const openCloseColors = ([open, close]) => open > close ? '#da344d' : '#32936f'
+      scaleX = scaleBand()
+        .domain(
+          timeDay
+            .range(domainDate[0], +domainDate[1] + 1)
+            .filter(d => d.getDay() !== 0 && d.getDay() !== 6)
+        )
+        .padding(0.2)
+      scaleY = scaleLog().domain([domainLow[0], domainHigh[1]])
+
+      xTicks = timeMonday.every(1).range(domainDate[0], domainDate[1])
+      scaleYAxis = scaleLinear()
+        .domain([domainLow[0], domainHigh[1]])
+        .range([height - padding.bottom, padding.top])
+    }
+  }
 </script>
 
-{#if ready}
+<Graphic {width} {height}>
 
-  <Graphic
-    width={500}
-    height={500}
-    padding={{ top: 20, bottom: 30, left: 40, right: 30 }}
-    {scaleX}
-    {scaleY}
-  >
+  {#if done}
+    <Section
+      {scaleX} 
+      {scaleY}
+      {padding}
+      flipY
+    >  
 
-    <LineLayer 
-      x={appleStockData.column('dates')}
-      y={appleStockData.column('lowHigh')}
-      strokeWidth={1}
-    />
+      <LineLayer
+        x={dataContainer.map('date', d => [d, d])}
+        y={dataContainer.rows().map(r => [r.low, r.high])}
+        strokeWidth={1}
+      />
 
-    <LineLayer
-      x={appleStockData.column('dates')}
-      y={appleStockData.column('openClose')}
-      stroke={appleStockData.map('openClose', openCloseColors)}
-      strokeWidth={4}
-    />
+      <LineLayer
+        x={dataContainer.map('date', d => [d, d])}
+        y={dataContainer.rows().map(r => [r.open, r.close])}
+        strokeWidth={4}
+        stroke={dataContainer.rows().map(r => r.open > r.close ? '#da344d'
+          : r.close > r.open ? '#32936f'
+          : '#32936f')}
+      />
 
-    <XAxis tickValues={ticksX} labelFormat={timeFormat('%-m/%-d')} baseLine={false} /> 
-    <YAxis labelFormat={format('$d')} baseLine={false} />
-  
-  </Graphic>
+      <XAxis tickValues={xTicks} labelFormat={timeFormat('%-m/%-d')} baseLine={false} /> 
+      <YAxis scale={scaleYAxis} labelFormat={format('$d')} baseLine={false} />
 
-{/if}
+    </Section>
+  {/if}
+
+</Graphic>
