14e0cb97caa2e24d8bd8decef1cd9fef4f6fce1b
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-12-01 17:21:40 +0100

    candlestick: further improve readability

--- site/examples/candlestick/App.svelte
+++ site/examples/candlestick/App.svelte
@@ -7,16 +7,15 @@
   import { Graphic, LineLayer, XAxis, YAxis } from '@snlab/florence'
   import DataContainer from '@snlab/florence-datacontainer'
 
-  const parseDate = timeParse('%Y-%m-%d')
-  const padding = { top: 20, bottom: 30, left: 40, right: 30 }
   const width = 800
   const height = 600
-  const dataURL = '/data/apple-stocks-candlestick.csv'
+  const padding = { top: 20, bottom: 30, left: 40, right: 30 }
+  const parseDate = timeParse('%Y-%m-%d')
   
   let dataContainer, scaleX, scaleY, xTicks, done
 
   (async () => {
-    const data = await csv(dataURL, d => ({
+    const data = await csv('/data/apple-stocks-candlestick.csv', d => ({
       Date:  parseDate(d.Date),
       High: +d.High,
       Low: +d.Low,
@@ -25,9 +24,12 @@
     }))
 
     dataContainer = new DataContainer(data.slice(-120))
+      .mutate({
+        dates: r => [r.Date, r.Date],
+        lowHigh: r => [r.Low, r.High],
+        openClose: r => [r.Open, r.Close]
+      })
 
-    const domainHigh = dataContainer.domain('High')
-    const domainLow = dataContainer.domain('Low')
     const domainDate = dataContainer.domain('Date')
 
     const weekdays = timeDay
@@ -38,56 +40,15 @@
       .domain(weekdays)
       .padding(0.2)
     
-    scaleY = scaleLog().domain([domainLow[0], domainHigh[1]])
+    scaleY = scaleLog().domain([
+      dataContainer.min('Low'),
+      dataContainer.max('High')
+    ])
 
     xTicks = timeMonday.every(1).range(domainDate[0], domainDate[1])
 
     done = true
   })()
-
-  // let data
-  // csv('/data/apple-stocks-candlestick.csv', d => {
-  //   const date = parseDate(d['Date'])
-  //   return {
-  //     Date: date,
-  //     High: +d['High'],
-  //     Low: +d['Low'],
-  //     Open: +d['Open'],
-  //     Close: +d['Close']
-  //   }
-  // }).then(d => {
-  //   data = d.slice(-120)
-  //   done = true
-  // })
-
-  // const padding = { top: 20, bottom: 30, left: 40, right: 30 }
-  // const width = 800
-  // const height = 600
-
-  // let dataContainer
-  // let scaleX, scaleY
-  // let xTicks
-
-  // $: {
-  //   if (done) {
-  //     dataContainer = new DataContainer(data)
-
-  //     const domainHigh = dataContainer.domain('High')
-  //     const domainLow = dataContainer.domain('Low')
-  //     const domainDate = dataContainer.domain('Date')
-
-  //     const weekdays = timeDay
-  //       .range(domainDate[0], +domainDate[1] + 1)
-  //       .filter(d => d.getDay() !== 0 && d.getDay() !== 6)
-
-  //     scaleX = scaleBand()
-  //       .domain(weekdays)
-  //       .padding(0.2)
-  //     scaleY = scaleLog().domain([domainLow[0], domainHigh[1]])
-
-  //     xTicks = timeMonday.every(1).range(domainDate[0], domainDate[1])
-  //   }
-  // }
 </script>
 
 {#if done}
@@ -102,16 +63,16 @@
   >
 
     <LineLayer
-      x={dataContainer.map('Date', d => [d, d])}
-      y={dataContainer.rows().map(r => [r.Low, r.High])}
+      x={dataContainer.column('dates')}
+      y={dataContainer.column('lowHigh')}
       strokeWidth={1}
     />
 
     <LineLayer
-      x={dataContainer.map('Date', d => [d, d])}
-      y={dataContainer.rows().map(r => [r.Open, r.Close])}
+      x={dataContainer.column('dates')}
+      y={dataContainer.column('openClose')}
       strokeWidth={4}
-      stroke={dataContainer.rows().map(r => r.Open > r.Close ? '#da344d' : '#32936f') }
+      stroke={dataContainer.map('openClose', r => r[0] > r[1] ? '#da344d' : '#32936f') }
     />
 
     <XAxis tickValues={xTicks} labelFormat={timeFormat('%-m/%-d')} baseLine={false} /> 
