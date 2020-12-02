3b411e3fb8ca43948e7f53fa8352d15b1a91b21b
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-12-02 10:58:55 +0100

    docs: examples for graphic and section

---
+++ site/src/routes/docs/core/examples/_SectionSimple.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Graphic, Section } from '@snlab/florence'
+</script>
+
+<Graphic width={200} height={200} backgroundColor={'blue'}>
+  
+  <Section 
+    x1={50}
+    x2={150}
+    y1={50}
+    y2={150}
+    backgroundColor={'green'}
+  >
+  
+  </Section>
+
+</Graphic>
