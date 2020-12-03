97a6210e13f15a27e8669c96ce57ed6993917ecd
Ate Poorthuis <atepoorthuis@gmail.com>
2020-12-03 23:13:53 +0100

    fix: simplify repl

---
+++ site/src/routes/repl/index.svelte
@@ -0,0 +1,35 @@
+<script>
+import REPL from '@snlab/florence-repl'
+import { onMount, onDestroy } from 'svelte'
+import appSource from './appSource.js'
+
+let containerWidth
+let windowHeight
+
+const replFiles = [
+  {
+    id: 0,
+    name: 'App',
+    type: 'svelte',
+    source: appSource
+  }
+]
+</script>
+
+<style>
+.repl {
+  @apply border border-gray-100;
+}
+</style>
+
+<svelte:window bind:innerHeight={windowHeight}/>
+<div class="repl" bind:clientWidth={containerWidth}>
+  {#if containerWidth}
+    <REPL
+      {replFiles}
+      width={containerWidth}
+      height={windowHeight * 0.8}
+      debounce={400}
+    />
+  {/if}
+</div>
