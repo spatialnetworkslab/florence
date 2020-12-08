d2b8c1192b798b59d559768ab1cd49cca2208d44
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-12-08 13:34:42 +0100

    docs: rewrote choropleth tutorial

--- site/src/routes/tutorials/utils/_Choropleth.svelte
+++ site/src/routes/tutorials/utils/_Choropleth.svelte
@@ -1,12 +1,14 @@
 <script>
-  import { geojson } from './planning_areas_data.js'
   import { 
     Graphic, Section, PolygonLayer, createGeoScales,
     Label, DiscreteLegend, getClassLabels
   } from '@snlab/florence'
   import DataContainer from '@snlab/florence-datacontainer'
+  import { geojson } from './planning_areas_data.js'
 
-  const COLORS = ['#fff0d2', '#FDD1A5', '#FD9243', '#982f05', '#4e1802']
+  export let switchColor = false
+  export let switchTitle = false
+  export let switchLegend = false
 
   const convertNullToUndefined = value => value === null ? undefined : value
 
@@ -15,6 +17,7 @@
 
   const geoScales = createGeoScales(dataContainer.bbox())
 
+  const COLORS = ['#fff0d2', '#FDD1A5', '#FD9243', '#982f05', '#4e1802']
   const priceColorScale = dataContainer
     .dropNA('resale_price_sqm')
     .classify(
@@ -27,42 +30,36 @@
 <Graphic width={500} height={500}>
 
   <Section
-    padding={30}
     {...geoScales}
     flipY
   >
     
     <PolygonLayer 
       geometry={dataContainer.column('$geometry')}
-      fill={dataContainer.map('resale_price_sqm', priceColorScale)}
+      fill={switchColor ? dataContainer.map('resale_price_sqm', priceColorScale) : '#d3d3d3'}
       stroke={'white'} 
       strokeWidth={1}
     />
 
   </Section>
 
-  <Label
-    x={250}
-    y={70}
-    text={'Mean resale price per m2 (S$)'}
-    fontFamily={'Montserrat'}
-    fontSize={18}
-  />
-
-  <DiscreteLegend
-    x1={300} x2={400}
-    y1={0} y2={100}
-    labels={getClassLabels(priceColorScale, Math.floor)}
-    fill={priceColorScale.range()}
-  >
-
+  {#if switchTitle}
     <Label
-      x={0.5}
-      y={0.1}
-      text={'Mean Resale Price / m2 (SGD)'}
-      fontSize={14}
+      x={250}
+      y={70}
+      text={'Mean resale price per m2 (S$)'}
+      fontFamily={'Montserrat'}
+      fontSize={18}
     />
+  {/if}
 
-  </DiscreteLegend>
+  {#if switchLegend}
+    <DiscreteLegend
+      x1={300} x2={400}
+      y1={300} y2={400}
+      labels={getClassLabels(priceColorScale, Math.floor)}
+      fill={COLORS}
+    />
+  {/if}
 
 </Graphic>
