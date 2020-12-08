0df560ec3ea65441380b1fea72094914b594a0e8
Ate Poorthuis <atepoorthuis@gmail.com>
2020-12-08 18:04:57 +0100

    fix: tutorial repl css

--- site/src/components/REPLWrapperTutorials.svelte
+++ site/src/components/REPLWrapperTutorials.svelte
@@ -9,25 +9,28 @@ let windowHeight
 
 <style>
 .repl {
-  left: 50%;
-	margin-left: -55vw;
-	margin-right: -55vw;
+  left: 0;
+	margin-left: 2vw;
+	margin-right: 2vw;
 	max-width: 96vw;
-	position: relative;
-	right: 50%;
+	position: absolute;
+	right: 0;
   background-color: white;
   @apply border border-gray-100;
 }
 </style>
 
 <svelte:window bind:innerHeight={windowHeight}/>
-<div class="repl" bind:clientWidth={containerWidth}>
-  {#if containerWidth}
-    <REPL
-      {replFiles}
-      width={containerWidth}
-      height={windowHeight * 0.8}
-      debounce={400}
-    />
-  {/if}
+
+<div>
+  <div class="repl" bind:clientWidth={containerWidth}>
+    {#if containerWidth}
+      <REPL
+        {replFiles}
+        width={containerWidth}
+        height={windowHeight * 0.8}
+        debounce={400}
+      />
+    {/if}
+  </div>
 </div>
