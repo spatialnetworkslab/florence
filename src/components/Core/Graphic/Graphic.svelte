1ea1d6db928765425f924ef82d7703de39b6626f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2021-04-26 11:52:33 +0200

    refactor: Graphic (now uses Section component instead of copying functionality)

--- src/components/Core/Graphic/Graphic.svelte
+++ src/components/Core/Graphic/Graphic.svelte
@@ -9,8 +9,8 @@
 <script>
   import { onMount, setContext } from 'svelte'
   import { writable } from 'svelte/store'
-  import { createSection, EventManager, InteractionManager } from 'rendervous'
-  import Clipper from '../Section/Clipper.svelte'
+  import { createSection, EventManager } from 'rendervous'
+  import Section from '../Section/Section.svelte'
 
   // Positioning
   export let width = 500
@@ -50,34 +50,9 @@
 
   // Initiate contexts
   const graphicContext = writable()
-  const sectionContext = writable()
   const eventManagerContext = writable()
-  const interactionManagerContext = writable()
   setContext('graphic', graphicContext)
-  setContext('section', sectionContext)
   setContext('eventManager', eventManagerContext)
-  setContext('interactionManager', interactionManagerContext)
-
-  // Section data
-  let section
-
-  $: {
-    section = createSection({
-      x1: 0,
-      x2: width,
-      y1: 0,
-      y2: height,
-      coordinates,
-      scaleX,
-      scaleY,
-      flipX,
-      flipY,
-      padding,
-      zoomIdentity,
-      clip,
-      id
-    })
-  }
 
   // Interactivity: set up globally
   const eventManager = new EventManager()
@@ -95,106 +70,47 @@
     eventManager.attachEventListeners()
   })
 
-  // Interactivity: component-specific
-  const interactionManager = new InteractionManager()
-
-  interactionManager.setId(id)
-  interactionManager.linkEventManager(eventManager)
-
-  $: {
-    interactionManager.loadSection(section)
-  }
-
-  $: {
-    removeSectionInteractionsIfNecessary(
-      onWheel,
-      onClick,
-      onMousedown,
-      onMouseup,
-      onMouseover,
-      onMouseout,
-      onTouchdown,
-      onTouchmove,
-      onTouchup,
-      onTouchover,
-      onTouchout,
-      onPinch
-    )
-  }
-
-  function removeSectionInteractionsIfNecessary () {
-    if (interactionManager.getPrimaryInput() === 'mouse') {
-      const sectionInterface = interactionManager.mouse().section()
-      sectionInterface.removeAllInteractions()
-
-      if (onWheel) sectionInterface.addInteraction('wheel', onWheel)
-      if (onClick) sectionInterface.addInteraction('click', onClick)
-      if (onMousedown) sectionInterface.addInteraction('mousedown', onMousedown)
-      if (onMouseup) sectionInterface.addInteraction('mouseup', onMouseup)
-      if (onMouseover) sectionInterface.addInteraction('mouseover', onMouseover)
-      if (onMouseout) sectionInterface.addInteraction('mouseout', onMouseout)
-      if (onMousemove) sectionInterface.addInteraction('mousemove', onMousemove)
-    }
-
-    if (interactionManager.getPrimaryInput() === 'touch') {
-      const sectionInterface = interactionManager.touch().section()
-      sectionInterface.removeAllInteractions()
-
-      if (onTouchdown) sectionInterface.addInteraction('touchdown', onTouchdown)
-      if (onTouchmove) sectionInterface.addInteraction('touchmove', onTouchmove)
-      if (onTouchup) sectionInterface.addInteraction('touchup', onTouchup)
-      if (onTouchover) sectionInterface.addInteraction('touchover', onTouchover)
-      if (onTouchout) sectionInterface.addInteraction('touchout', onTouchout)
-      if (onPinch) sectionInterface.addInteraction('pinch', onPinch)
-    }
-  }
-
-  export function selectRectangle (rectangle) {
-    interactionManager.select().selectRectangle(rectangle)
-  }
-
-  export function updateSelectRectangle (rectangle) {
-    interactionManager.select().updateSelectRectangle(rectangle)
-  }
-
-  export function resetSelectRectangle () {
-    interactionManager.select().resetSelectRectangle()
-  }
-
-  export function startSelectPolygon (startCoordinates) {
-    interactionManager.select().startSelectPolygon(startCoordinates)
-  }
-
-  export function addPointToSelectPolygon (pointCoordinates) {
-    interactionManager
-      .select()
-      .addPointToSelectPolygon(pointCoordinates)
-  }
-
-  export function moveSelectPolygon (delta) {
-    interactionManager.select().moveSelectPolygon(delta)
-  }
-
-  export function getSelectPolygon () {
-    return interactionManager.select().getSelectPolygon()
-  }
-
-  export function resetSelectPolygon () {
-    interactionManager.select().resetSelectPolygon()
-  }
-
-  // Expose contexts
+    // Expose contexts
   $: { graphicContext.set({ renderer, rootNode }) }
-  $: { sectionContext.set(section) }
   $: { eventManagerContext.set(eventManager) }
-  $: { interactionManagerContext.set(interactionManager) }
 </script>
 
 {#if renderer === 'svg'}
 
   <svg {id} {width} {height} bind:this={rootNode}>
-    <Clipper {section} />
-    <slot />
+
+    <Section
+      x1={0}
+      x2={width}
+      y1={0}
+      y2={height}
+      {coordinates}
+      {scaleX}
+      {scaleY}
+      {flipX}
+      {flipY}
+      {padding}
+      {zoomIdentity}
+      {onClick}
+      {onWheel}
+      {onMousedown}
+      {onMouseup}
+      {onMouseover}
+      {onMouseout}
+      {onMousemove}
+      {onPinch}
+      {onTouchdown}
+      {onTouchmove}
+      {onTouchup}
+      {onTouchover}
+      {onTouchout}
+      {clip}
+    >
+    
+      <slot />
+
+    </Section>
+
   </svg>
 
 {/if}
@@ -202,6 +118,37 @@
 {#if renderer === 'canvas'}
 
   <canvas {id} {width} {height} bind:this={rootNode} />
-  <slot />
+  
+  <Section
+    x1={0}
+    x2={width}
+    y1={0}
+    y2={height}
+    {coordinates}
+    {scaleX}
+    {scaleY}
+    {flipX}
+    {flipY}
+    {padding}
+    {zoomIdentity}
+    {onClick}
+    {onWheel}
+    {onMousedown}
+    {onMouseup}
+    {onMouseover}
+    {onMouseout}
+    {onMousemove}
+    {onPinch}
+    {onTouchdown}
+    {onTouchmove}
+    {onTouchup}
+    {onTouchover}
+    {onTouchout}
+    {clip}
+  >
+    
+    <slot />
+
+  </Section>
 
 {/if}
