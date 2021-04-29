63a01296571554c9551e166b1cafef00e9628ee2
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2021-04-29 22:20:33 +0200

    _

---
+++ src/components/Core/Graphic/RedrawManager.js
@@ -0,0 +1,54 @@
+export default class RedrawManager {
+  constructor () {
+    this.sections = {}
+  }
+
+  addSection (section, parentSection) {
+    this.sections[section.id] = {
+      data: section,
+      parentSection: parentSection.id,
+      childSections: new Set()
+    }
+
+    this.sections[parentSection.id].childSections.add(section.id)
+  }
+
+  removeSection (section) {
+    const parentId = this.sections[section.id].parentSection
+    delete this.sections[section.id]
+    this.sections[parentId].childSections.delete(parentId)
+  }
+
+  addMark (mark, section) {
+
+  }
+
+  removeMark (mark) {
+
+  }
+
+  addLayer (layer, section) {
+
+  }
+
+  removeLayer (layer) {
+
+  }
+
+  rerender (section) {
+
+  }
+}
+
+// https://stackoverflow.com/a/32088787/7237112
+// function sectionsOverlap (section1, section2) {
+//   const bbox1 = section1.bbox
+//   const bbox2 = section2.bbox
+
+//   return (
+//     bbox1.minX < bbox2.maxX &&
+//     bbox1.maxX > bbox2.minX &&
+//     bbox1.minY < bbox2.maxY &&
+//     bbox1.maxY > bbox2.minY
+//   )
+// }
