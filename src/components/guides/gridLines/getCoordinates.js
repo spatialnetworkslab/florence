620719ea8e024c56039047a9796296ff6aa1eb49
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2021-05-07 10:48:14 +0200

    _

--- src/components/guides/gridLines/getCoordinates.js
+++ src/components/guides/gridLines/getCoordinates.js
@@ -4,7 +4,7 @@ export function getCoordinatesXRaster (positions, scaleX, sectionContext) {
   const bandOffset = scaleX.bandwidth ? scaleX.bandwidth() / 2 : 0
 
   const x = positions.map(p => scaleX(p) + bandOffset).map(p => [p, p])
-  const y = generateArrayOfLength(sectionContext.rangeY, positions.length)
+  const y = generateArrayOfLength(sectionContext.ranges.y, positions.length)
 
   return {
     x: () => x,
@@ -15,7 +15,7 @@ export function getCoordinatesXRaster (positions, scaleX, sectionContext) {
 export function getCoordinatesYRaster (positions, scaleY, sectionContext) {
   const bandOffset = scaleY.bandwidth ? scaleY.bandwidth() / 2 : 0
 
-  const x = generateArrayOfLength(sectionContext.rangeX, positions.length)
+  const x = generateArrayOfLength(sectionContext.ranges.x, positions.length)
   const y = positions.map(p => scaleY(p) + bandOffset).map(p => [p, p])
 
   return {
