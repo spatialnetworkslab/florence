2c2ac73dd08669693938e9b71ad2f0624007c314
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2021-05-11 16:58:19 +0200

    chore: clean up utilities and dependencies

--- test/unit/RedrawManager.test.js
+++
@@ -1,7 +0,0 @@
-// import RedrawManager from '../../src/components/Core/Graphic/RedrawManager.js'
-
-describe('RedrawManager: no overlapping sections, no caching', () => {
-  it('correctly rerenders (1 layer, 1 section)', () => {
-
-  })
-})
