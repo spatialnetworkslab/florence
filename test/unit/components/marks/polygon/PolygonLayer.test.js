fdc5aea9b4aede059353da114a592ca8708cbe87
luucvanderzee <luuc_v_d_zee@hotmail.com>
2021-10-11 20:22:57 +0200

    test: more sanity tests Polygon/PolygonLayer

--- test/unit/components/marks/polygon/PolygonLayer.test.js
+++ test/unit/components/marks/polygon/PolygonLayer.test.js
@@ -1,8 +1,8 @@
 import { renderLayer } from '../../../utils.js'
 import { PolygonLayer } from '../../../../../src/index.js'
 
-describe('PointLayer (svg)', () => {
-  it('renders', () => {
+describe('PolygonLayer (svg)', () => {
+  it('renders with x/y coordinates', () => {
     const { getByTestId, getAllByTestId } = renderLayer(
       PolygonLayer,
       {
@@ -13,5 +13,48 @@ describe('PointLayer (svg)', () => {
 
     expect(getByTestId('polygon-layer')).toBeInTheDocument()
     expect(getAllByTestId('polygon-layer-mark').length).toEqual(2)
+  })
+
+  it('renders with geojson', () => {
+    const geometry1 = {
+      type: 'Polygon',
+      coordinates: [
+        [[[0.2, 0.2], [0.8, 0.2], [0.5, 0.5], [0.2, 0.2]]]
+      ]
+    }
+
+    const geometry2 = {
+      type: 'Polygon',
+      coordinates: [
+        [[[0.2, 0.8], [0.8, 0.8], [0.5, 0.5], [0.2, 0.8]]]
+      ]
+    }
+
+    const { getByTestId, getAllByTestId } = renderLayer(
+      PolygonLayer,
+      { geometry: [geometry1, geometry2] }
+    )
+
+    expect(getByTestId('polygon-layer')).toBeInTheDocument()
+    expect(getAllByTestId('polygon-layer-mark').length).toEqual(2)
+  })
+
+  it('calls onClick when clicked', () => {
+    const onClick = jest.fn()
+
+    const { dummyRoot } = renderLayer(
+      PolygonLayer,
+      {
+        x: [[0.2, 0.8, 0.5, 0.2], [0.2, 0.8, 0.5, 0.2]],
+        y: [[0.2, 0.2, 0.5, 0.2], [0.8, 0.8, 0.5, 0.8]],
+        onClick
+      }
+    )
+
+    dummyRoot.trigger('click', 0, 250)
+    dummyRoot.trigger('click', 250, 200)
+    dummyRoot.trigger('click', 250, 300)
+
+    expect(onClick).toHaveBeenCalledTimes(2)
   })
 })
