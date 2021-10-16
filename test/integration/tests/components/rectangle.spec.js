9762e1ae3127f367d560630e137bd42388c5cd0b
luucvanderzee <luuc_v_d_zee@hotmail.com>
2021-10-16 19:03:31 +0200

    test: fixed integration tests rectangle

--- test/integration/tests/components/rectangle.spec.js
+++ test/integration/tests/components/rectangle.spec.js
@@ -7,19 +7,14 @@ context('Assertions', () => {
 
   describe('Rectangle Tests', () => {
     it('[render] rectangles rendered', () => {
-      cy.get('.rectangle').should('have.length', 3)
-    })
-
-    it('[render] rectangles snapshot correctly', () => {
-      cy.get('svg')
-        .toMatchSnapshot()
+      cy.get('.rectangle-layer > path').should('have.length', 3)
     })
 
     it('[interaction] clicking on rectangle changes color', () => {
-      var rectangle = cy.get('.rectangle').first()
-      rectangle.should('have.attr', 'fill', 'yellow')
+      const rectangle = cy.get('.rectangle-layer > path').first()
+      rectangle.should('have.attr', 'fill', 'rgba(255,255,0,1)')
       rectangle.click()
-      rectangle.should('have.attr', 'fill', 'blue')
+      rectangle.should('have.attr', 'fill', 'rgba(0,0,255,1)')
     })
   })
 })
