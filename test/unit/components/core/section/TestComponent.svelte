22d78210b2b32bfc59a960679b9b1f44f109dbf5
luucvanderzee <luuc_v_d_zee@hotmail.com>
2021-10-17 17:16:14 +0200

    test: added sanity tests for Section

---
+++ test/unit/components/core/section/TestComponent.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { Graphic, Section, Point } from '../../../../../src/index.js'
+
+  export let graphic = {}
+  export let section = {}
+  export let point = {}
+
+  $: _graphic = Object.assign({ width: 500, height: 500 }, graphic)
+</script>
+
+<Graphic {..._graphic}>
+  <Section {...section}>
+    <Point {...point} />
+  </Section>
+</Graphic>
