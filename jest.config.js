f0aeaca8551543d75c66c9ca3c7cc0e41ed6bb27
luucvanderzee <luuc_v_d_zee@hotmail.com>
2021-10-30 14:38:20 +0200

    tests: fix

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest'
   },
   transformIgnorePatterns: [
-    '<rootDir>/node_modules'
+    'node_modules/(?!(@snlab/rendervous|d3-path|d3-scale|d3-shape|d3-array|d3-interpolate|d3-color|d3-format|d3-time|d3-time-format|internmap)/)'
   ],
   testPathIgnorePatterns: [
     '/__data__/'
