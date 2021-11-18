d4693d386b85658ec98d4f3f8dd11a38ac516e52
luucvanderzee <luuc_v_d_zee@hotmail.com>
2021-11-18 23:09:39 +0100

    refactor: update examples

--- test/sandbox/src/examples/GridLayout.svelte
+++ test/sandbox/src/examples/GridLayout.svelte
@@ -33,21 +33,17 @@
 
 <div>
 
-	<Graphic
-		width={500} height={500}
-	>
+	<Graphic width={500} height={500} padding={50}>
 
 		<Grid
-			x1={0.1} x2={0.9}
-			y1={0.1} y2={0.9}
-			names={groupedData.domain('color')}
+			numberOfCells={groupedData.domain('color').length}
 			let:cells
 		>
 
-			{#each groupedData.rows() as facet (facet.$key)}
+			{#each groupedData.rows() as facet, i (facet.$key)}
 
 				<Section
-					{...cells[facet.color]}
+					{...cells[i]}
 					scaleX={scaleA}
 					scaleY={scaleB}
 				>
@@ -73,85 +69,3 @@
 	</Graphic>
 
 </div>
-
-<!-- <script>
-	import { scaleLinear, scaleBand } from 'd3-scale'
-	import { Graphic, Section, Point, Rectangle, Grid } from '../../../../src/'
-	import DataContainer from '@snlab/florence-datacontainer'
-
-	let cols = 2
-
-	function generateData (N) {
-  	let newData = { a: [], b: [], color: [] }
-  	let allColors = ['red', 'blue', 'green', 'orange']
-
-  	for (let i = 0; i < N; i++) {
-  		let cat = Math.floor(Math.random() * 4)
-
-  		newData.color.push(allColors[cat])
-  		newData.a.push(Math.floor(Math.random() * 50) + 25)
-  		newData.b.push(Math.floor(Math.random() * 50))
-  	}
-
-  	return newData
-  }
-
-  let data = new DataContainer(generateData(50))
-
-  const scaleA = scaleLinear().domain(data.domain('a'))
-  const scaleB = scaleLinear().domain(data.domain('b'))
-
-  const groupedData = data.groupBy('color')
-
-  $: rows = Math.ceil(4/cols)
-</script>
-
-<div>
-  <label for="cols-slider">Columns:</label>
-  <input type="range" min="1" max="4" bind:value={cols} name="cols-slider" />
-</div>
-
-<div>
-
-	<Graphic 
-		width={500} height={500}
-	>
-
-		<Grid
-			x1={0.1} x2={0.9}
-			y1={0.1} y2={0.9}
-			columns={cols}
-			rows={rows}
-			names={groupedData.domain('color')}
-			let:cells
-		>
-
-			{#each groupedData.rows() as facet (facet.$key)}
-
-				<Section
-					{...cells[facet.color]}
-					scaleX={scaleA}
-					scaleY={scaleB}
-				>
-
-          <Rectangle fill={facet.color} opacity={0.4} />
-
-					{#each facet.$grouped.rows() as row (row.$key)}
-
-						<Point 
-							x={row.a}
-							y={row.b} 
-							fill={row.color}
-						/>
-
-					{/each}
-		
-				</Section>
-
-			{/each}
-		
-		</Grid>
-
-	</Graphic>
-
-</div> -->
